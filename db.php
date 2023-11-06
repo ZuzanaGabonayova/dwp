a62bbaec76a48af3eee302236a426e884e579928
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-11-06 14:27:36 +0100

    testing

--- db.php
+++ db.php
@@ -4,11 +4,11 @@ $username = "zuzanagabonayova_euwebshopdb"; // default XAMPP username
 $password = "dwp2023"; // default XAMPP password
 $dbname = "zuzanagabonayova_euwebshopdb";
 
-/* // Create connection
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} */
+}
 ?>
