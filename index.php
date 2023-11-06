9e1195e028c7a69046185b7b9c602aa5dafcda56
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-11-06 14:29:33 +0100

    testing

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
+
 <h1>hello</h1>
 
 <!DOCTYPE html>
