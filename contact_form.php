0a730d0baa77d4beed764a9b2e18269e1634d027
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-07 11:43:39 +0100

    update contact form ux

--- contact_form.php
+++ contact_form.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-<form class="mx-auto max-w-xl" action="contact.php" method="post" onsubmit="submitForm(event)">
+<form class="mx-auto max-w-xl" id="contactForm" action="contact.php" method="post">
 <div class="mb-10">
     <h2>Contact Us</h2>
 </div>
@@ -28,5 +28,30 @@
     <button class="block w-full rounded-md bg-[#FF8C42] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-[#FF8C42]/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="submit" value="Submit"> Send message </button>
 </div>
 </form>
+<div id="formResponse"></div>
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('contactForm');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submit action
+
+        var formData = new FormData(form);
+        fetch('contact.php', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.text())
+        .then(text => {
+            document.getElementById('formResponse').innerText = text;
+            // Clear the form if needed, or handle UI changes
+            form.reset();
+        })
+        .catch(error => {
+            document.getElementById('formResponse').innerText = 'An error occurred: ' + error;
+        });
+    });
+});
+</script>
+
 </body>
 </html>
