5385d00187df5e0b54f0815d61cc64e7c3c876cc
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-07 11:55:32 +0100

    update contact form

--- contact.php
+++ contact.php
@@ -5,6 +5,8 @@ use PHPMailer\PHPMailer\Exception;
 require 'vendor/autoload.php'; // If using Composer
 require 'db.php'; // Your database connection file
 
+// The rest of your
+
 // Function to sanitize form input
 function test_input($data) {
   $data = trim($data);
@@ -13,21 +15,16 @@ function test_input($data) {
   return $data;
 }
 
-header('Content-Type: application/json'); // Specify the content type as JSON
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Sanitize input
     $name = test_input($_POST["name"]);
     $email = test_input($_POST["email"]);
     $message = test_input($_POST["message"]);
 
-    // Save to database
+   // Save to database
     $stmt = $conn->prepare("INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)");
     $stmt->bind_param("sss", $name, $email, $message);
-    if (!$stmt->execute()) {
-        echo json_encode(['status' => 'error', 'message' => 'Could not save to database.']);
-        exit;
-    }
+    $stmt->execute();
     $stmt->close();
 
     // Send email using PHPMailer
@@ -35,14 +32,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     try {
         // Server settings
-        // ... your existing code ...
+        $mail->isSMTP();
+        $mail->Host       = 'send.one.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'info@zuzanagabonayova.eu';
+        $mail->Password   = 'dwp2023'; // Replace with your password
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port       = 465;
+
+        // Recipients
+        $mail->setFrom('info@zuzanagabonayova.eu', 'Website Contact Form');
+        $mail->addAddress('vitkai.laca1@gmail.com'); // Replace with your email
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Contact Form Submission';
+        $mail->Body    = "You have received a new message from $name.<br>Email: $email<br>Message: $message";
+        $mail->AltBody = "You have received a new message from $name.\nEmail: $email\nMessage: $message";
 
         $mail->send();
-        echo json_encode(['status' => 'success', 'message' => 'Message has been sent.']);
+        echo 'Message has been sent';
     } catch (Exception $e) {
-        echo json_encode(['status' => 'error', 'message' => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"]);
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 } else {
-    echo json_encode(['status' => 'error', 'message' => 'Request must be POST.']);
+    header('Location: contact_form.php');
 }
 ?>
--- contact_form.php
+++ contact_form.php
@@ -32,8 +32,6 @@
 <script>
 document.addEventListener('DOMContentLoaded', function() {
     var form = document.getElementById('contactForm');
-    var formResponse = document.getElementById('formResponse');
-
     form.addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent the default form submit action
 
@@ -42,22 +40,18 @@ document.addEventListener('DOMContentLoaded', function() {
             method: 'POST',
             body: formData
         })
-        .then(response => response.json()) // Parse the JSON response
-        .then(data => {
-            // Handle the response based on the status
-            formResponse.innerText = data.message;
-            if (data.status === 'success') {
-                // Clear the form or any other success actions
-                form.reset();
-            }
+        .then(response => response.text())
+        .then(text => {
+            document.getElementById('formResponse').innerText = text;
+            // Clear the form if needed, or handle UI changes
+            form.reset();
         })
         .catch(error => {
-            formResponse.innerText = 'An error occurred: ' + error;
+            document.getElementById('formResponse').innerText = 'An error occurred: ' + error;
         });
     });
 });
 </script>
-
 
 </body>
 </html>
