2a02e1df554f7fe0e38389d8477f5e8538a83a87
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-09 15:23:29 +0100

    update index.php with links

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <title>Document</title>
 </head>
 <body>
-    <a href="product_list.php">Product list</a>
+    <a href="list_product.php">Product list</a>
+    <a href="news_post_list.php">News post list</a>
 </body>
 </html>
