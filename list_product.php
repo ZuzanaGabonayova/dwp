5e1e56380ba541e075b6137fd76ea1746bcd516e
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-09 11:41:45 +0100

    add line-clamp to table

--- list_product.php
+++ list_product.php
@@ -82,7 +82,7 @@ function getAuthorName($AdminID, $conn) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product List</title>
     <!-- Include Tailwind CSS from CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
     <div class="container mx-auto px-4">
