9d400a409caa5fc81e8a79c4a3e69a6f19764911
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-11-09 22:39:36 +0100

    navigation

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
     <title>Document</title>
 </head>
 <body>
-    <a href="list_product.php">Product list</a>
-    <a href="news_post_list.php">News post list</a>
+    <a href="list_product.php">Product list</a><br>
+    <a href="news_post_list.php">News post list</a><br>
+    <a href="contact_form.php">Contact form</a><br>
+    <a href="visitor_product_page.php">Visitor products</a><br>
+    <a href="news.php">Visitor news</a><br>
 </body>
 </html>
