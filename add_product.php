fffc71d08e6bec4e175fcfa9f4abefc1c0f95f88
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-09 15:07:09 +0100

    create add product page

---
+++ add_product.php
@@ -0,0 +1,101 @@
+<?php
+require 'db.php';
+require 'upload.php';
+
+$categories = $conn->query("SELECT CategoryID, CategoryName FROM ProductCategory")->fetch_all(MYSQLI_ASSOC);
+$brands = $conn->query("SELECT BrandID, BrandName FROM ProductBrand")->fetch_all(MYSQLI_ASSOC);
+$colors = $conn->query("SELECT ColorID, ColorName FROM Color")->fetch_all(MYSQLI_ASSOC);
+$sizes = $conn->query("SELECT SizeID, Size FROM Size")->fetch_all(MYSQLI_ASSOC);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $productNumber = $_POST["ProductNumber"];
+    $model = $_POST["Model"];
+    $description = $_POST["Description"];
+    $price = $_POST["Price"];
+    $stockQuantity = $_POST["StockQuantity"];
+    $categoryID = $_POST["CategoryID"];
+    $brandID = $_POST["BrandID"];
+    $selectedColors = $_POST["colors"] ?? [];
+    $selectedSizes = $_POST["sizes"] ?? [];
+    $mainImage = '';
+
+    if (isset($_FILES["ProductMainImage"]) && $_FILES["ProductMainImage"]["error"] === UPLOAD_ERR_OK) {
+        $uploadResult = uploadFile($_FILES["ProductMainImage"]);
+        if (isset($uploadResult['success'])) {
+            $mainImage = $uploadResult['success'];
+        } else {
+            $error = $uploadResult['error'];
+        }
+    }
+
+    if (!isset($error)) {
+        $stmt = $conn->prepare("INSERT INTO Product (ProductNumber, Model, Description, Price, StockQuantity, CategoryID, BrandID, ProductMainImage) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssdiiis", $productNumber, $model, $description, $price, $stockQuantity, $categoryID, $brandID, $mainImage);
+        $stmt->execute();
+        $productID = $conn->insert_id;
+
+        $colorStmt = $conn->prepare("INSERT INTO ProductColor (ProductID, ColorID) VALUES (?, ?)");
+        foreach ($selectedColors as $colorID) {
+            $colorStmt->bind_param("ii", $productID, $colorID);
+            $colorStmt->execute();
+        }
+
+        $sizeStmt = $conn->prepare("INSERT INTO ProductSize (ProductID, SizeID) VALUES (?, ?)");
+        foreach ($selectedSizes as $sizeID) {
+            $sizeStmt->bind_param("ii", $productID, $sizeID);
+            $sizeStmt->execute();
+        }
+
+        header("Location: list_product.php?add=success");
+        exit();
+    }
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add New Product</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100">
+    <div class="container mx-auto px-4">
+        <h1 class="text-xl font-semibold text-gray-800 my-6">Add New Product</h1>
+        <form action="add_product.php" method="post" enctype="multipart/form-data" class="mb-4">
+            <input type="text" name="ProductNumber" placeholder="Product Number" required>
+            <input type="text" name="Model" placeholder="Model" required>
+            <textarea name="Description" placeholder="Description" required></textarea>
+            <input type="number" name="Price" step="0.01" placeholder="Price" required>
+            <input type="number" name="StockQuantity" placeholder="Stock Quantity" required>
+            <select name="CategoryID" required>
+                <option value="">Select Category</option>
+                <?php foreach ($categories as $category): ?>
+                <option value="<?= $category["CategoryID"]; ?>"><?= htmlspecialchars($category["CategoryName"]); ?></option>
+                <?php endforeach; ?>
+            </select>
+            <select name="BrandID" required>
+                <option value="">Select Brand</option>
+                <?php foreach ($brands as $brand): ?>
+                <option value="<?= $brand["BrandID"]; ?>"><?= htmlspecialchars($brand["BrandName"]); ?></option>
+                <?php endforeach; ?>
+            </select>
+            <?php foreach ($colors as $color): ?>
+            <label>
+                <input type="checkbox" name="colors[]" value="<?= $color["ColorID"]; ?>"><?= htmlspecialchars($color["ColorName"]); ?>
+            </label>
+            <?php endforeach; ?>
+            <?php foreach ($sizes as $size): ?>
+            <label>
+                <input type="checkbox" name="sizes[]" value="<?= $size["SizeID"]; ?>"><?= htmlspecialchars($size["Size"]); ?>
+            </label>
+            <?php endforeach; ?>
+            <input type="file" name="ProductMainImage">
+            <input type="submit" value="Add Product" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+        </form>
+    </div>
+</body>
+</html>
