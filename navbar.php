ad97d46181155b3d118c4cedb0d8a1231ba47c26
Klaudia Purdiakova <claudiapur9@gmail.com>
2023-11-10 11:59:13 +0100

    hamburger menu script

--- navbar.php
+++ navbar.php
@@ -126,6 +126,19 @@ $cartItemCount = isset($_SESSION['cart']) ? array_sum(array_column($_SESSION['ca
 
     </nav>
 
+    <script>
+        const menu_btn = document.getElementById('menu_btn')
+        const mobile_menu = document.getElementById('mobile_menu')
+        const menu_bars = document.getElementById('menu_bars')
+        const menu_close = document.getElementById('menu_close')
+
+        menu_btn.addEventListener('click', () => {
+            mobile_menu.classList.toggle('hidden')
+            menu_bars.classList.toggle('hidden')
+            menu_close.classList.toggle('hidden')
+        })
+    </script>
+
 </body>
 
 </html>
