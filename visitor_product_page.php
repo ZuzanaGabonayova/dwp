5f64717fe8aa3f60aa13872b26cd56372ff0638f
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-11-10 11:55:41 +0100

    design changes

--- visitor_product_page.php
+++ visitor_product_page.php
@@ -119,7 +119,7 @@ include 'navbar.php';
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="bg-gray-100">
+<body class="bg-white">
 
     <?php
     include 'inc/hero.php';
