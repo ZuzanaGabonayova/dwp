b29a207a8b0d2feae777fffb49bcffa211864e94
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-11-10 11:33:48 +0100

    removed top_categories part from navbarto inc folder

--- navbar.php
+++ navbar.php
@@ -76,66 +76,6 @@ $cartItemCount = isset($_SESSION['cart']) ? array_sum(array_column($_SESSION['ca
     </nav>
 
 
-
-    <!-- Your page content goes here -->
-
-    <div class="flex justify-center items-center">
-        <div class="w-10/12">
-            <div>
-
-                <h1 class="font-bold text-4xl">
-                    OUR TOP CATEGORIES
-                </h1>
-                <h3>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim
-                </h3>
-            </div>
-            <div class="w-full flex justify-center">
-                <div class="w-9/12 flex items-center justify-between">
-                    <a href="#"> NIKE DUNK </a>
-                    <a href="news.php"> AIR FORCE 1 </a>
-                    <a href="product_list.php"> JORDAN 1 LOW </a>
-                    <a href="product_list.php"> JORDAN 1 MID </a>
-                    <a href="product_list.php"> JORDAN 1 HIGH </a>
-                    <a href="product_list.php"> YEEZY </a>
-
-
-                </div>
-
-
-
-
-            </div>
-
-            <div class="w-full grid grid-cols-3 gap-20">
-                <div class="border-2 border-solid rounded-3xl shadow-lg h-[500px]
-
-">
-                    <div class="flex justify-center border-b-2 border-solid shadow-lg 
-
-">
-                        <img class="object-cover h-72 w-[450px]" src="assets/images/prod_654698358ed90.png" />
-                    </div>
-                    <div class="flex justify-center align-center w-full">
-                        <div class="my-4 w-10/12">
-                            <p class="font-bold">Nike Dunk Low Arizona State</p>
-
-                            <div class="mt-10 flex flex-col border-b-2 border-solid border-[#FF8C42] w-24 items-center">
-                                <div class="my-2">
-                                    <p class="py-2">250 €</p>
-                                    <span class="text-[#FF8C42] font-bold ">DETAILS</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-
-
     <nav class="flex justify-between items-center px-10 py-5 relative">
         <a class="font-black text-xl" href="">LOGO</a>
 
