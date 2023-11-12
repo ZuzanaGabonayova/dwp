26142d5d9a52ae5154f673c21cbf601a12d6ca3f
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-12 21:22:26 +0100

    add comments for better readability

--- visitor_product_page.php
+++ visitor_product_page.php
@@ -29,11 +29,11 @@ include 'navbar.php';
 </head>
 
 <body class="bg-white">
-
+     <!-- Hero section -->
     <?php
     include 'inc/hero.php';
     ?>
-
+        <!-- Products Grid -->
         <div class="bg-white">
             <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
@@ -68,48 +68,5 @@ include 'navbar.php';
                 </div>
             </div>
         </div>
-        <!-- Products Grid -->
-        <div class="bg-white">
-            <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                <h2 class="text-3xl font-bold tracking-tight text-gray-900">
-                    Our top categoriess
-                </h2>
-                <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-                <?php if ($products) : ?>
-                <?php while ($product = $products->fetch_assoc()) : ?>
-                    <div class="group relative bg-white p-4 shadow-lg rounded-md">
-                        <div
-                        class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75"
-                        >
-                            <img
-                            class="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                            src="<?php echo htmlspecialchars($product['ProductMainImage']); ?>"
-                            alt="<?php echo htmlspecialchars($product['Model']); ?>" 
-                            />
-                        </div>
-                        <div class="mt-4 flex justify-between">
-                            <div>
-                                <h3 class="text-sm text-[#FF8C42] font-bold">
-                                <a href="<?php echo baseUrl(); ?>single_product.php?ProductID=<?php echo $product['ProductID']; ?>">
-                                    <span aria-hidden="true" class="absolute inset-0"></span>
-                                    <?php echo htmlspecialchars($product['Model']); ?>
-                                </a>
-                                </h3>
-                            </div>
-                            <p class="text-sm font-medium text-gray-900"><?php echo htmlspecialchars(number_format($product['Price'], 2)); ?> kr.</p>
-                        </div>
-                    </div>
-                <?php endwhile; ?>
-
-                <?php else : ?>
-                    <p>No products found.</p>
-                <?php endif; ?>
-
-                </div>
-            </div>
-        </div>
-    
-
 </body>
-
 </html>
