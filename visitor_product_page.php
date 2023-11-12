f6fc6eea2d31fe18f63f9e12b6900c143148d3b8
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-12 21:11:47 +0100

    update single product cards

--- visitor_product_page.php
+++ visitor_product_page.php
@@ -34,7 +34,33 @@ include 'navbar.php';
     include 'inc/hero.php';
     ?>
 
-    
+        <div class="bg-white">
+            <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+
+                <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                <div class="group relative">
+                    <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img 
+                    src="<?php echo htmlspecialchars($product['ProductMainImage']); ?>"
+                    alt="<?php echo htmlspecialchars($product['Model']); ?>"
+                    class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                    <div>
+                        <h3 class="text-sm text-[#FF8C42] font-bold">
+                        <a href="<?php echo baseUrl(); ?>single_product.php?ProductID=<?php echo $product['ProductID']; ?>">
+                            <span aria-hidden="true" class="absolute inset-0"></span>
+                            <?php echo htmlspecialchars($product['Model']); ?>
+                        </a>
+                        </h3>
+                    </div>
+                    <p class="text-sm font-medium text-gray-900"><?php echo htmlspecialchars(number_format($product['Price'], 2)); ?> kr.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
         <!-- Products Grid -->
         <div class="bg-white">
             <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -58,7 +84,7 @@ include 'navbar.php';
                             <div>
                                 <h3 class="text-sm text-[#FF8C42] font-bold">
                                 <a href="<?php echo baseUrl(); ?>single_product.php?ProductID=<?php echo $product['ProductID']; ?>">
-                                <span aria-hidden="true" class="absolute inset-0"></span>
+                                    <span aria-hidden="true" class="absolute inset-0"></span>
                                     <?php echo htmlspecialchars($product['Model']); ?>
                                 </a>
                                 </h3>
