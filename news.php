39dc2d2d3728a59402cf38ad4531390ea01e579f
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-13 20:07:37 +0100

    removed old news section design

--- news.php
+++ news.php
@@ -17,39 +17,7 @@ include 'navbar.php';
     <title>News</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-gray-100">
-
-<div class="container mx-auto p-6">
-    <div class="flex justify-between items-center pb-6">
-        <div>
-            <h2 class="text-4xl font-semibold">Latest News</h2>
-        </div>
-    </div>
-
-    <!-- News Grid -->
-    <div class="grid md:grid-cols-3 gap-6">
-        <?php if ($newsPosts && $newsPosts->num_rows > 0): ?>
-            <?php while ($newsPost = $newsPosts->fetch_assoc()): ?>
-                <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-                    <img class="w-full" src="<?php echo htmlspecialchars($newsPost['image']); ?>" alt="<?php echo htmlspecialchars($newsPost['image_alt']); ?>">
-                    <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2"><?php echo htmlspecialchars($newsPost['title']); ?></div>
-                        <p class="text-gray-700 text-base">
-                            <?php echo htmlspecialchars($newsPost['short_description']); ?>
-                        </p>
-                    </div>
-                    <div class="px-6 py-4">
-                        <?php echo nl2br(htmlspecialchars($newsPost['content'])); ?>
-                    </div>
-                </div>
-            <?php endwhile; ?>
-        <?php else: ?>
-            <p class="text-center">No news posts found.</p>
-        <?php endif; ?>
-    </div>
-</div>
-
-
+<body class="">
 <div class="bg-white py-24 sm:py-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl text-center">
