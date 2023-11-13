ddd3ead4d6cf94d1e0ae3bcb3e733ba40c57765f
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-13 20:15:22 +0100

    changed description in the news section

--- news.php
+++ news.php
@@ -22,7 +22,7 @@ include 'navbar.php';
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl text-center">
       <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-      <p class="mt-2 text-lg leading-8 text-gray-600">Learn how to grow your business with our expert advice.</p>
+      <p class="mt-2 text-lg leading-8 text-gray-600">Learn about the newest trends in the world of sneakers.</p>
     </div>
     <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
       <?php if ($newsPosts && $newsPosts->num_rows > 0): ?>
