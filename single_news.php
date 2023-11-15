ec42f2cd727c6f7db89ea32c094fe093f8e3285c
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-15 20:32:29 +0100

    added tailwind css globally

--- single_news.php
+++ single_news.php
@@ -16,14 +16,17 @@ $conn->close();
     <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:max-w-7xl lg:gap-x-8 lg:px-8 py-16 sm:py-24">
         <?php if ($news): ?>
             <div class="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                <div class="aspect-square rounded-lg ">
-                    <img src="<?= htmlspecialchars($news['image']) ?>" alt="<?= htmlspecialchars($news['image_alt']) ?>" class="h-full w-full object-cover object-center">
-                </div>
-                <div class="px-8 mt-20 lg:mt-0 sm:px:0 sm:mt-16 ">
-                    <h1 class="text-3xl font-bold mb-2 tracking-tight"><?= htmlspecialchars($news['title']) ?></h1>
-                    <p><?= nl2br(htmlspecialchars($news['content'])) ?></p>
-                    <p class="mt-1">Posted on <?= htmlspecialchars($news['created_at']) ?></p>
-                </div>
+                <article class="prose lg:prose-xl">
+                    <div class="aspect-square rounded-lg ">
+                        <img src="<?= htmlspecialchars($news['image']) ?>" alt="<?= htmlspecialchars($news['image_alt']) ?>" class="h-full w-full object-cover object-center">
+                    </div>
+                    <div class="px-8 mt-20 lg:mt-0 sm:px:0 sm:mt-16 ">
+                        <h1 class="text-3xl font-bold mb-2 tracking-tight"><?= htmlspecialchars($news['title']) ?></h1>
+                        <p><?= nl2br(htmlspecialchars($news['content'])) ?></p>
+                        <p class="mt-1">Posted on <?= htmlspecialchars($news['created_at']) ?></p>
+                    </div>
+                </article>
+                
             </div>
         <?php else: ?>
             <p class="text-center">News post not found.</p>
