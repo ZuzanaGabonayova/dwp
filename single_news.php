7cf0b0219236e9745dab4d36ebe134f913c3d37a
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-15 20:39:13 +0100

    added tailwind script

--- single_news.php
+++ single_news.php
@@ -19,6 +19,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="output.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <div>
