6008763c7fb045b5c9945706dfd8efba2e599129
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-19 22:14:25 +0100

    update modal z indexing

--- list_product.php
+++ list_product.php
@@ -23,9 +23,8 @@ function baseUrl() {
     <link rel="stylesheet" href="output.css">
 </head>
 <body class="">
-    <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true" id="productModal" style="display: none;">
-    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-    <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+    <div class="fixed inset-0 z-100 bg-gray-500 bg-opacity-75 transition-opacity" aria-labelledby="modal-title" role="dialog" aria-modal="true" id="productModal" style="display: none;">
+  
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
                 <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
@@ -47,7 +46,7 @@ function baseUrl() {
                 </div>
             </div>
         </div>
-    </div>
+    
     </div>
 
     <div class="bg-gray-100 py-10">
