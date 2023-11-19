e60ebd19adcc3eaf855efab4aad5527274d720a1
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-19 21:20:10 +0100

    update add product modal

--- list_product.php
+++ list_product.php
@@ -23,26 +23,15 @@ function baseUrl() {
     <link rel="stylesheet" href="output.css">
 </head>
 <body class="">
-<div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-  <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-  <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-    <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-      <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-        <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-            <div class="id="productModal"">
-                <div id="modalContent"><!-- Content will be loaded here --></div>
-            </div>
-        </div>
-        <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-          <div class="mt-4">
+    <!-- Modal structure (hidden initially) -->
+    <div id="productModal" class="hidden fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
+        <div class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+            <div id="modalContent"><!-- Content will be loaded here --></div>
+            <div class="mt-4">
                 <button onclick="closeModal()" class="px-4 py-2 bg-red-500 text-white rounded">Close</button>
             </div>
         </div>
-      </div>
     </div>
-  </div>
-</div>
-    
     <div class="bg-gray-100 py-10">
         <div class="max-w-7xl mx-auto">
             <div class="px-4 sm:px-6 lg:px-8">
