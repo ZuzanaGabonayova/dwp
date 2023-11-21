a2a9a26b2d94c94eb4e35183a419a67701fecc4c
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-21 22:34:45 +0100

    update add photo field

--- add_product.php
+++ add_product.php
@@ -176,8 +176,9 @@ $conn->close();
                                 </div>
                                 <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
                             </div>
-                            <input type="file" name="ProductMainImage">
+                            
                     </div>
+                    <input type="file" name="ProductMainImage">
                     <div class="mt-6 flex items-center justify-end">
                         <input type="submit" value="Add Product" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">                
                     </div>
