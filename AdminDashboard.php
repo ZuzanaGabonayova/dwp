4a8efd8b279666cc218afe059c0b8612b4815d17
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-11-22 17:42:50 +0100

    refactored admin dashboard js part

--- AdminDashboard.php
+++ AdminDashboard.php
@@ -300,12 +300,13 @@
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             document.getElementById("content-wrapper").innerHTML = xhr.responseText;
+            // Load the modal HTML from add_product.php
+            loadModalContent();
             reInitEventHandlers(); // Reinitialize event handlers
         }
     };
     xhr.send();
-}
-
+  }
 
   // Select the 'Products' link and add event listener
   var productsLink = document
@@ -319,9 +320,49 @@
       }
     });
 
-    
+  // Function to load the Add Product modal
+function loadModalContent() {
+    var xhrModal = new XMLHttpRequest();
+    xhrModal.open("GET", "add_product.php", true);
+    xhrModal.onreadystatechange = function () {
+        if (xhrModal.readyState == 4 && xhrModal.status == 200) {
+            document.getElementById("content-wrapper").innerHTML += xhrModal.responseText;
+            // Attach event listener to the Add Product button
+            attachModalButtonListener();
+        }
+    };
+    xhrModal.send();
+}
+
+// Attach event listener to the Add Product button
+function attachModalButtonListener() {
+    let showModalBtn = document.getElementById("showModalBtn");
+    if (showModalBtn) {
+        showModalBtn.addEventListener("click", function(e) {
+            e.preventDefault();
+            showModal(); // Function to show the modal
+        });
+    }
+}
+
+// Show modal
+    function showModal() {
+        document.getElementById("addProductModal").style.display = "block";
+        document.addEventListener("click", handleClickOutside, true);
+    }
+
+// Hide modal
+    function hideModal() {
+        document.getElementById("addProductModal").style.display = "none";
+        // Optionally, focus back to showModalBtn if exists
+        let showModalBtn = document.getElementById("showModalBtn");
+        if (showModalBtn) {
+            showModalBtn.focus();
+        }
+        document.removeEventListener("click", handleClickOutside, true);
+    }
 </script>
-  </body>
+</body>
 </html>
 
 
