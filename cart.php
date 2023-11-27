f17781c26eeef5384712fa4ad13ca9d8d733e8ce
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-11-27 10:23:05 +0100

    shopping cart two column design

--- cart.php
+++ cart.php
@@ -73,6 +73,28 @@ if (isset($_GET["action"])) {
     $shouldRedirect = true; // Set the redirection flag
 }
 
+$productDetails = array(); // Initialize an array to hold product details
+
+if (!empty($_SESSION["shopping_cart"])) {
+    foreach ($_SESSION["shopping_cart"] as $key => $value) {
+        $productId = $value['item_id'];
+
+        // Fetch product details from the database based on the product ID
+        $sql = "SELECT * FROM Products WHERE ProductID = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $productId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            // Store fetched product details in the array
+            $productDetails[$key] = $row;
+            $productDetails[$key]['quantity'] = $value['item_quantity']; // Add quantity to product details
+        }
+    }
+}
+
 // Redirect to prevent form resubmission
 if ($shouldRedirect) {
     header('Location: cart.php'); // Redirect only when needed
@@ -91,63 +113,70 @@ if ($shouldRedirect) {
     <?php include './inc/navigationbar.php'; ?>
     
     <!-- Shopping Cart content -->
-    <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:max-w-7xl lg:gap-x-8 lg:px-8 py-16 sm:py-24 bg-white">
-        <h1 class="text-3xl font-bold mb-2 tracking-tight">Shopping Cart</h1>
-        <table class="table-auto w-full">
-            <thead>
-                <!-- Table headers -->
-            </thead>
-            <tbody>
-                <?php
-                // Loop through cart items
-                if (!empty($_SESSION["shopping_cart"])) {
-                    $total = 0;
-                    foreach ($_SESSION["shopping_cart"] as $key => $value) {
-                        ?>
-                        <tr>
-                            <!-- Product details -->
-                            <td class="border px-4 py-2"><?= $value["item_name"]; ?></td>
-                            <td class="border px-4 py-2">$<?= $value["item_price"]; ?></td>
-                            <td class="border px-4 py-2">
-                                <!-- Quantity and buttons -->
-                                <span><?= $value["item_quantity"]; ?></span>
-                                <form method="GET" action="cart.php">
-                                    <input type="hidden" name="action" value="increase">
-                                    <input type="hidden" name="id" value="<?= $value["item_id"]; ?>">
-                                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded" name="increase">+</button>
-                                </form>
-                                <form method="GET" action="cart.php">
-                                    <input type="hidden" name="action" value="decrease">
-                                    <input type="hidden" name="id" value="<?= $value["item_id"]; ?>">
-                                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded" name="decrease">-</button>
-                                </form>
-                            </td>
-                            <!-- Total price for each item -->
-                            <td class="border px-4 py-2">$<?= number_format($value["item_quantity"] * $value["item_price"], 2); ?></td>
-                            <!-- Actions -->
-                            <td class="border px-4 py-2">
-                                <form method="GET" action="cart.php">
-                                    <input type="hidden" name="action" value="delete">
-                                    <input type="hidden" name="id" value="<?= $value["item_id"]; ?>">
-                                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded" name="delete">Remove</button>
-                                </form>
-                            </td>
-                        </tr>
-                        <?php
-                        $total += ($value["item_quantity"] * $value["item_price"]);
-                    }
-                    ?>
-                    <tr>
-                        <!-- Total row -->
-                        <td colspan="3" align="right">Total</td>
-                        <td align="right">$<?= number_format($total, 2); ?></td>
-                        <td></td>
-                    </tr>
-                    <?php
-                }
-                ?>
-            </tbody>
-        </table>
+    <div class="container mx-auto px-4 py-8">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+            <div class="bg-white p-4 shadow-md rounded-md">
+                <h1 class="text-2xl font-bold mb-4">Shopping Cart</h1>
+                <div class="space-y-4">
+                    <?php foreach ($productDetails as $key => $product): ?>
+                        <div class="flex items-center border-b pb-4">
+                            <img src="<?= htmlspecialchars($product['ProductMainImage']) ?>" alt="Product Image" class="w-20 h-20 rounded-md object-cover mr-4">
+                            <div class="flex-1">
+                                <h2 class="text-lg font-semibold"><?= htmlspecialchars($product['Model']) ?></h2>
+                                <p class="text-gray-600">Price: $<?= htmlspecialchars($product['Price']) ?></p>
+                                <p class="text-gray-600">Size: <?= htmlspecialchars($product['Size']) ?></p>
+                                <select class="mt-2 p-2 border border-gray-300 rounded-md" name="quantity">
+                                    <?php for ($i = 1; $i <= 10; $i++): ?>
+                                        <option value="<?= $i ?>" <?= ($i == $product['quantity']) ? 'selected' : '' ?>><?= $i ?></option>
+                                    <?php endfor; ?>
+                                </select>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+            <div class="bg-white p-4 shadow-md rounded-md">
+    <h1 class="text-2xl font-bold mb-4">Order Summary</h1>
+    <?php
+    // Calculate subtotal
+    $subtotal = 0;
+    foreach ($productDetails as $product) {
+        $subtotal += $product['Price'] * $product['quantity'];
+    }
+
+    // Calculate tax (assuming 20%)
+    $tax = $subtotal * 0.2;
+
+    // Define shipping fee (you can set your own logic for shipping fee calculation)
+    $shippingFee = 10; // Example: $10 shipping fee
+
+    // Calculate total order price
+    $totalOrderPrice = $subtotal + $tax + $shippingFee;
+    ?>
+    
+    <div class="flex justify-between mb-4">
+        <span>Subtotal:</span>
+        <span>$<?= number_format($subtotal, 2) ?></span>
+    </div>
+    
+    <div class="flex justify-between mb-4">
+        <span>Shipping Fee:</span>
+        <span>$<?= number_format($shippingFee, 2) ?></span>
+    </div>
+    
+    <div class="flex justify-between mb-4">
+        <span>Tax (20%):</span>
+        <span>$<?= number_format($tax, 2) ?></span>
+    </div>
+    
+    <div class="flex justify-between border-t pt-4">
+        <span class="font-bold">Total Order Price:</span>
+        <span class="font-bold">$<?= number_format($totalOrderPrice, 2) ?></span>
+    </div>
+    
+    <button class="mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Checkout</button>
+</div>
+        </div>
     </div>
 
 </body>
