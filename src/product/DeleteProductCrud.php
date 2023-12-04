53d1dc53be64f74ca00ff8c91c79158c25c64d81
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-04 16:17:39 +0100

    update rquire once links

--- src/product/DeleteProductCrud.php
+++ src/product/DeleteProductCrud.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../../config/db.php';
+require_once 'src/config/db.php';
 
 class DeleteProductCrud {
     private $conn;
--- src/product/UpdateProductCrud.php
+++ src/product/UpdateProductCrud.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . '/../../config/db.php';
-require_once __DIR__ . '/../../utils/upload.php';
+require_once 'src/config/db.php';
+require_once 'src/utils/upload.php';
 
 class UpdateProductCrud {
     private $conn;
