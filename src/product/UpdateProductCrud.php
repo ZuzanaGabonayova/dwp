5477e91095d96318203fe75367cde561a3651780
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-04 14:40:48 +0100

    refactored update_product.php and added the crud file for it

---
+++ src/product/UpdateProductCrud.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once 'db.php'; 
+require_once './src/utils/upload.php';
+
+class UpdateProductCrud {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+      public function getCategories() {
+        return $this->conn->query("SELECT CategoryID, CategoryName FROM ProductCategory")->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getBrands() {
+        return $this->conn->query("SELECT BrandID, BrandName FROM ProductBrand")->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getColors() {
+        return $this->conn->query("SELECT ColorID, ColorName FROM Color")->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getSizes() {
+        return $this->conn->query("SELECT SizeID, Size FROM Size")->fetch_all(MYSQLI_ASSOC);
+    }
+
+
+    public function readProduct($productId) {
+        $stmt = $this->conn->prepare("SELECT * FROM Product WHERE ProductID = ?");
+        $stmt->bind_param("i", $productId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->num_rows > 0 ? $result->fetch_assoc() : false;
+    }
+
+    public function getCurrentAttributes($productId, $attributeTable, $idField) {
+        $stmt = $this->conn->prepare("SELECT $idField FROM $attributeTable WHERE ProductID = ?");
+        $stmt->bind_param("i", $productId);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        return array_column($result, $idField);
+    }
+
+    public function updateProduct($formData, $fileData, $productId) {
+        // Extract and sanitize data from $formData
+        $productNumber = $formData["ProductNumber"];
+        $model = $formData["Model"];
+        $description = $formData["Description"];
+        $price = $formData["Price"];
+        $stockQuantity = $formData["StockQuantity"];
+        $categoryID = $formData["CategoryID"];
+        $brandID = $formData["BrandID"];
+        $selectedColors = $formData["colors"] ?? [];
+        $selectedSizes = $formData["sizes"] ?? [];
+
+        // Process file upload
+        if (isset($fileData["ProductMainImage"]) && $fileData["ProductMainImage"]["error"] === UPLOAD_ERR_OK) {
+            $uploadResult = uploadFile($fileData["ProductMainImage"]);
+            if (isset($uploadResult['success'])) {
+                $mainImage = $uploadResult['success'];
+                $stmt = $this->conn->prepare("UPDATE Product SET ProductMainImage = ? WHERE ProductID = ?");
+                $stmt->bind_param("si", $mainImage, $productId);
+                $stmt->execute();
+            } else {
+                return ['error' => $uploadResult['error']];
+            }
+        }
+
+        // Update product data in the database
+        $stmt = $this->conn->prepare("UPDATE Product SET ProductNumber = ?, Model = ?, Description = ?, Price = ?, StockQuantity = ?, CategoryID = ?, BrandID = ? WHERE ProductID = ?");
+        $stmt->bind_param("sssdiiii", $productNumber, $model, $description, $price, $stockQuantity, $categoryID, $brandID, $productId);
+        $stmt->execute();
+
+        // Update color associations
+        $this->conn->query("DELETE FROM ProductColor WHERE ProductID = $productId");
+        $colorStmt = $this->conn->prepare("INSERT INTO ProductColor (ProductID, ColorID) VALUES (?, ?)");
+        foreach ($selectedColors as $colorID) {
+            $colorStmt->bind_param("ii", $productId, $colorID);
+            $colorStmt->execute();
+        }
+
+        // Update size associations
+        $this->conn->query("DELETE FROM ProductSize WHERE ProductID = $productId");
+        $sizeStmt = $this->conn->prepare("INSERT INTO ProductSize (ProductID, SizeID) VALUES (?, ?)");
+        foreach ($selectedSizes as $sizeID) {
+            $sizeStmt->bind_param("ii", $productId, $sizeID);
+            $sizeStmt->execute();
+        }
+
+        return ['success' => true];
+    }
+}
--- update_product.php
+++ update_product.php
@@ -1,86 +1,31 @@
 <?php
-require 'db.php';
-require 'src/utils/upload.php';
+require 'src/product/UpdateProductCrud.php';
 
-function readProduct($productId, $conn) {
-    $stmt = $conn->prepare("SELECT * FROM Product WHERE ProductID = ?");
-    $stmt->bind_param("i", $productId);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if ($result->num_rows > 0) {
-        return $result->fetch_assoc();
-    } else {
-        return false;
-    }
-}
+$crud = new UpdateProductCrud($conn);
 
-function getCurrentAttributes($productId, $conn, $attributeTable, $idField) {
-    $stmt = $conn->prepare("SELECT $idField FROM $attributeTable WHERE ProductID = ?");
-    $stmt->bind_param("i", $productId);
-    $stmt->execute();
-    $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-    return array_column($result, $idField);
-}
-
-$categories = $conn->query("SELECT CategoryID, CategoryName FROM ProductCategory")->fetch_all(MYSQLI_ASSOC);
-$brands = $conn->query("SELECT BrandID, BrandName FROM ProductBrand")->fetch_all(MYSQLI_ASSOC);
-$colors = $conn->query("SELECT ColorID, ColorName FROM Color")->fetch_all(MYSQLI_ASSOC);
-$sizes = $conn->query("SELECT SizeID, Size FROM Size")->fetch_all(MYSQLI_ASSOC);
+$categories = $crud->getCategories();
+$brands = $crud->getBrands();
+$colors = $crud->getColors();
+$sizes = $crud->getSizes();
 
 $product = false;
 $currentColors = $currentSizes = [];
 if (isset($_GET["ProductID"])) {
     $productID = intval($_GET["ProductID"]);
-    $product = readProduct($productID, $conn);
-    $currentColors = getCurrentAttributes($productID, $conn, 'ProductColor', 'ColorID');
-    $currentSizes = getCurrentAttributes($productID, $conn, 'ProductSize', 'SizeID');
+    $product = $crud->readProduct($productID);
+    $currentColors = $crud->getCurrentAttributes($productID, 'ProductColor', 'ColorID');
+    $currentSizes = $crud->getCurrentAttributes($productID, 'ProductSize', 'SizeID');
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["ProductID"])) {
-    $productID = $_POST["ProductID"];
-    $productNumber = $_POST["ProductNumber"];
-    $model = $_POST["Model"];
-    $description = $_POST["Description"];
-    $price = $_POST["Price"];
-    $stockQuantity = $_POST["StockQuantity"];
-    $categoryID = $_POST["CategoryID"];
-    $brandID = $_POST["BrandID"];
-    $selectedColors = $_POST["colors"] ?? [];
-    $selectedSizes = $_POST["sizes"] ?? [];
-
-    if (isset($_FILES["ProductMainImage"]) && $_FILES["ProductMainImage"]["error"] === UPLOAD_ERR_OK) {
-        $uploadResult = uploadFile($_FILES["ProductMainImage"]);
-        if (isset($uploadResult['success'])) {
-            $mainImage = $uploadResult['success'];
-            $stmt = $conn->prepare("UPDATE Product SET ProductMainImage = ? WHERE ProductID = ?");
-            $stmt->bind_param("si", $mainImage, $productID);
-            $stmt->execute();
-        }
+    $result = $crud->updateProduct($_POST, $_FILES, $_POST["ProductID"]);
+    if (isset($result['error'])) {
+        $error = $result['error'];
+    } else {
+        header("Location: list_product.php?update=success");
+        exit();
     }
-
-    $stmt = $conn->prepare("UPDATE Product SET ProductNumber = ?, Model = ?, Description = ?, Price = ?, StockQuantity = ?, CategoryID = ?, BrandID = ? WHERE ProductID = ?");
-    $stmt->bind_param("sssdiiii", $productNumber, $model, $description, $price, $stockQuantity, $categoryID, $brandID, $productID);
-    $stmt->execute();
-
-    $conn->query("DELETE FROM ProductColor WHERE ProductID = $productID");
-    $colorStmt = $conn->prepare("INSERT INTO ProductColor (ProductID, ColorID) VALUES (?, ?)");
-    foreach ($selectedColors as $colorID) {
-        $colorStmt->bind_param("ii", $productID, $colorID);
-        $colorStmt->execute();
-    }
-
-    $conn->query("DELETE FROM ProductSize WHERE ProductID = $productID");
-    $sizeStmt = $conn->prepare("INSERT INTO ProductSize (ProductID, SizeID) VALUES (?, ?)");
-    foreach ($selectedSizes as $sizeID) {
-        $sizeStmt->bind_param("ii", $productID, $sizeID);
-        $sizeStmt->execute();
-    }
-
-    header("Location: list_product.php?update=success");
-    exit();
 }
-
-$conn->close();
 ?>
 
 <!DOCTYPE html>
