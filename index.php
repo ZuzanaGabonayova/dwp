4b8b72c1273c6b19bf9ffc271066866f734ac232
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-05 11:10:08 +0100

    links

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
     <title>Document</title>
 </head>
 <body>
-    <a href="src/views/list_product.php">Product list</a><br>
-    <a href="src/views/news_post_form.php">News post list</a><br>
-    <a href="src/forms/contact.php">Contact form</a><br>
-    <a href="src/views/visitor_product_page.php">Visitor products</a><br>
+    <a href="./src/views/list_product.php">Product list</a><br>
+    <a href="./src/views/news_post_form.php">News post list</a><br>
+    <a href="./src/forms/contact.php">Contact form</a><br>
+    <a href="./src/views/visitor_product_page.php">Visitor products</a><br>
     <a href="news.php">Visitor news</a><br>
     <a href="AdminDashboard.php">Admin dashboard</a><br>
 </body>
