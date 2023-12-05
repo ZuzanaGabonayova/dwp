9a4ab30b1188a84dc23d34d9832911b20cc103d4
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-05 22:41:02 +0100

    remove product from cart

--- src/views/cart.php
+++ src/views/cart.php
@@ -198,7 +198,7 @@ if ($shouldRedirect) {
                                     <form method="post" action="cart.php">
                                         <input type="hidden" name="action" value="delete">
                                         <input type="hidden" name="id" value="<?= $product['ProductID'] ?>">
-                                        <button type="submit" class="r-[-0.5rem] text-gray400 p-2" name="remove_item" action="delete">
+                                        <button type="submit" class="r-[-0.5rem] text-gray400 p-2" name="remove_item">
                                             <span class="sr-only">Remove</span>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-5 w-5">
                                                 <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"></path>
