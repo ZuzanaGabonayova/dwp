ed0509221e8ee9e03499f3157753f048afa979cb
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-05 23:45:46 +0100

    navbar products in cart counting

--- src/components/navigationbar.php
+++ src/components/navigationbar.php
@@ -47,15 +47,15 @@
                   </svg>
                 </a>
               </div>
-        <div class="ml-4 flow-root lg:ml-6">
+              <div class="ml-4 flow-root lg:ml-6">
                 <a href="#" class="group -m-2 flex items-center p-2">
-                  <svg class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"></path>
-                 </svg>
-                  <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
-                  <span class="sr-only">items in cart, view bag</span>
+                    <svg class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"></path>
+                    </svg>
+                    <span id="cartCount" class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+                    <span class="sr-only">items in cart, view bag</span>
                 </a>
-        </div>
+            </div>
     </div>
   </nav>
   <!-- Mobile menu, show/hide based on menu open state. -->
@@ -94,6 +94,24 @@
 </header>
 
 <script>
+  document.addEventListener('DOMContentLoaded', function() {
+    const cartCount = document.getElementById('cartCount');
+
+    // Function to update cart count
+    function updateCartCount() {
+        // Get the number of items in the shopping cart session variable
+        const shoppingCart = <?php echo !empty($_SESSION["shopping_cart"]) ? json_encode($_SESSION["shopping_cart"]) : '[]'; ?>;
+        const itemCount = shoppingCart.length;
+
+        // Update the cart count displayed in the navbar
+        cartCount.textContent = itemCount;
+    }
+
+    // Call the function initially to set the initial cart count
+    updateCartCount();
+});
+
+
  document.addEventListener('DOMContentLoaded', function() {
     const menuOpenButton = document.getElementById('menu-open-button');
     const menuCloseButton = document.getElementById('menu-close-button');
