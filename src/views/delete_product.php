102dbe95731207f3d7e6cafd86ec97d61d0008ca
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-05 12:51:41 +0100

    link fix

--- src/views/delete_product.php
+++ src/views/delete_product.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 
-require './product/DeleteProductCrud.php';
+require '../product/DeleteProductCrud.php';
 
 $crud = new DeleteProductCrud($conn);
 
