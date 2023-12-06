425718521454be56f00bbb93ea8d17062be6ed00
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-06 12:45:51 +0100

    update company handle file

--- src/actions/handle_update_company_presentation.php
+++ src/actions/handle_update_company_presentation.php
@@ -8,6 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Get the form data
     $description = $_POST['description'];
+    $openingHours = $_POST['openingHours'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $street = $_POST['street'];
+    $houseNumber = $_POST['houseNumber'];
+    $postalCodeID = $_POST['postalCodeID'];
     // Retrieve other fields similarly
 
     // Update the company presentation
