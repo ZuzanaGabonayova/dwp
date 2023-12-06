df822455aeacb37cbb316054f3e531fb33ca0e70
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-06 13:08:22 +0100

    update handle to delete news

---
+++ src/actions/handle_delete_news_post.php
@@ -0,0 +1,23 @@
+<?php
+require_once '../config/db.php';
+require_once '../news/DeleteNewsCrud.php';
+
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $newsId = $_GET['id'];
+
+    $deleteCrud = new DeleteNewsCrud($conn);
+    if ($deleteCrud->deleteNewsPost($newsId)) {
+        // Redirect to a confirmation page or the list of news posts
+        header('Location: all_news_posts.php?message=News post deleted successfully');
+        exit();
+    } else {
+        // Handle deletion error
+        header('Location: all_news_posts.php?error=Error deleting news post');
+        exit();
+    }
+} else {
+    // Handle invalid deletion request
+    header('Location: all_news_posts.php?error=Invalid request');
+    exit();
+}
+?>
