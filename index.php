d5f51d002614a0cd4088c0ffc7ddf2e0f780e2aa
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-06 20:53:30 +0100

    links

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <a href="./src/views/all_news_posts.php">All news posts</a><br>
     <a href="./src/components/contact_form.php">Contact form</a><br>
     <a href="./src/views/visitor_product_page.php">Visitor products</a><br>
-    <a href="news.php">Visitor news</a><br>
-    <a href="AdminDashboard.php">Admin dashboard</a><br>
+    <a href="./src/views/news.php">Visitor news</a><br>
+    <a href="./src/views/AdminDashboard.php">Admin dashboard</a><br>
 </body>
 </html>
