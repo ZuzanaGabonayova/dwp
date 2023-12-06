444471ffe5f76d4a3d69bc3117ed16f64bfdf5a6
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-06 15:41:06 +0100

    refactored read product functions

--- src/product/ReadProductCrud.php
+++ src/product/ReadProductCrud.php
@@ -2,105 +2,100 @@
 
 require_once '../config/db.php';
 
-// Read all products
-function readProducts() {
-    global $conn;
-    
-    $sql = "SELECT * FROM Product";
-    $result = $conn->query($sql);
-    
-    if ($result->num_rows > 0) {
-        return $result;
-    } else {
-        return false;
+class ReadProductCrud {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    // Read all products
+    public function readProducts() {
+        $sql = "SELECT * FROM Product";
+        $result = $this->conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    // Read a single product by its ProductID
+    public function readProduct($productID) {
+        $stmt = $this->conn->prepare("SELECT * FROM Product WHERE ProductID = ?");
+        $stmt->bind_param("i", $productID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($product = $result->fetch_assoc()) {
+            return $product;
+        } else {
+            return null; // No product found with the given ProductID
+        }
+    }
+
+    // Function to get color names for a product
+    public function getProductColors($productId) {
+        $colors = [];
+        $sql = "SELECT c.ColorName FROM `ProductColor` pc
+                JOIN `Color` c ON pc.ColorID = c.ColorID
+                WHERE pc.ProductID = " . intval($productId);
+        $result = $this->conn->query($sql);
+        while ($row = $result->fetch_assoc()) {
+            $colors[] = $row["ColorName"];
+        }
+        return $colors;
+    }
+
+    // Function to get size names for a product
+    public function getProductSizes($productId) {
+        $sizes = [];
+        $sql = "SELECT s.Size FROM `ProductSize` ps
+                JOIN `Size` s ON ps.SizeID = s.SizeID
+                WHERE ps.ProductID = " . intval($productId);
+        $result = $this->conn->query($sql);
+        while ($row = $result->fetch_assoc()) {
+            $sizes[] = $row["Size"];
+        }
+        return $sizes;
+    }
+
+    // Function to get the category name for a product
+    public function getCategoryName($categoryId) {
+        $sql = "SELECT CategoryName FROM `ProductCategory` WHERE CategoryID = " . intval($categoryId);
+        $result = $this->conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            return $row["CategoryName"];
+        } else {
+            return null;
+        }
+    }
+
+    // Function to get the brand name for a product
+    public function getBrandName($brandId) {
+        $sql = "SELECT BrandName FROM `ProductBrand` WHERE BrandID = " . intval($brandId);
+        $result = $this->conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            return $row["BrandName"];
+        } else {
+            return null;
+        }
+    }
+
+    // Function to get the author name for a product
+    public function getAuthorName($AdminID) {
+        $sql = "SELECT FirstName, LastName FROM Admin WHERE AdminID = " . intval($AdminID);
+        $result = $this->conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            return $row["FirstName"] . " " . $row["LastName"];
+        } else {
+            return null;
+        }
     }
 }
-
-// Read a single product by its ProductID
-function readProduct($productID) {
-    global $conn;
-    
-    // Prepare the SQL statement to prevent SQL injection
-    $stmt = $conn->prepare("SELECT * FROM Product WHERE ProductID = ?");
-    $stmt->bind_param("i", $productID);
-    
-    // Execute the statement and get the result
-    $stmt->execute();
-    $result = $stmt->get_result();
-    
-    // Fetch the product data
-    if ($product = $result->fetch_assoc()) {
-        return $product;
-    } else {
-        return null; // No product found with the given ProductID
-    }
-}
-
-// Function to get color names for a product
-function getProductColors($productId, $conn) {
-    $colors = [];
-    $sql = "SELECT c.ColorName FROM `ProductColor` pc
-            JOIN `Color` c ON pc.ColorID = c.ColorID
-            WHERE pc.ProductID = " . intval($productId);
-    $result = $conn->query($sql);
-    while ($row = $result->fetch_assoc()) {
-        $colors[] = $row["ColorName"];
-    }
-    return $colors;
-}
-
-
-// Function to get size names for a product
-function getProductSizes($productId, $conn) {
-    $sizes = [];
-    $sql = "SELECT s.Size FROM `ProductSize` ps
-            JOIN `Size` s ON ps.SizeID = s.SizeID
-            WHERE ps.ProductID = " . intval($productId);
-    $result = $conn->query($sql);
-    while ($row = $result->fetch_assoc()) {
-        $sizes[] = $row["Size"];
-    }
-    return $sizes;
-}
-
-// Function to get the category name for a product
-function getCategoryName($categoryId, $conn) {
-    $sql = "SELECT CategoryName FROM `ProductCategory` WHERE CategoryID = " . intval($categoryId);
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        return $row["CategoryName"];
-    } else {
-        return null;
-    }
-}
-
-// Function to get the brand name for a product
-function getBrandName($brandId, $conn) {
-    $sql = "SELECT BrandName FROM `ProductBrand` WHERE BrandID = " . intval($brandId);
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        return $row["BrandName"];
-    } else {
-        return null;
-    }
-}
-
-// Function to get the author name for a product
-function getAuthorName($AdminID, $conn) {
-    // This assumes you have a table named `Authors` with fields `AuthorID` and `AuthorName`
-    // Adjust the table and field names according to your schema
-    $sql = "SELECT FirstName, LastName FROM Admin WHERE AdminID = " . intval($AdminID);
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        return $row["FirstName"] . " " . $row["LastName"];
-    } else {
-        return null;
-    }
-}
-
 
 ?>
-
--- src/views/list_product.php
+++ src/views/list_product.php
@@ -1,12 +1,10 @@
 <?php
-
 require_once '../config/db.php';
 require_once '../product/ReadProductCrud.php';
 require_once '../utils/url_helpers.php';
 
-// Attempt to fetch all products
-$products = readProducts();
-
+$readProductCrud = new ReadProductCrud($conn);
+$products = $readProductCrud->readProducts();
 ?>
 
 <!DOCTYPE html>
@@ -17,110 +15,72 @@ $products = readProducts();
     <title>Product List</title>
     <link rel="stylesheet" href="../../assets/css/output.css">
 </head>
-<body class="">
-
-    <div class="bg-gray-100 py-10">
-        <div class="max-w-7xl mx-auto">
-            <div class="px-4 sm:px-6 lg:px-8">
-                <div class="sm:flex sm:items-center">
-                    <div class="sm:flex-auto">
-                        <h1 class="text-base font-semibold leading-6 text-gray-900">Products</h1>
-                        <p class="mt-2 text-sm text-gray-700">A list of all the products in the webshop. Including their attributes.</p>
-                    </div>
-                    <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                        <a href="add_product.php" class="block rounded-md bg-amber-500 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm">Add product</a>
-                    </div>
-                </div>
-                <div class="mt-8 flow-root ">
-                    <div class="mx-4 -my-2 overflow-x-auto sm:mx-6 lg:-mx-8">
-                        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                            <div class="overflow-hidden border border-gray-300 sm:rounded-lg">
-                                <table class="min-w-full">
-                                    <thead class="bg-gray-50">
-                                        <tr>
-                                            <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Number</th>
-                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Name</th>
-                                          
-                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Price</th>
-                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Image</th>
-                                            <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                                                <span class="sr-only">Actions</span>
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody class="bg-white divide-y divide-gray-300">
-                                       <?php
-                                        // SQL query to select all records from the Product table
-                                        $sql = "SELECT * FROM `Product`";
-
-                                        // Execute the query
-                                        $result = $conn->query($sql);
-
-                                        // Check if there are results
-                                        if ($result->num_rows > 0):
-                                            // Output data of each row
-                                            while($product = $result->fetch_assoc()):
-                                                $productColors = getProductColors($product["ProductID"], $conn);
-                                                $productSizes = getProductSizes($product["ProductID"], $conn);
-                                                $categoryName = getCategoryName($product["CategoryID"], $conn);
-                                                $brandName = getBrandName($product["BrandID"], $conn);
-                                                $authorName = getAuthorName($product["AdminID"], $conn); // If you have an authors table
-                                            ?>
-                                        <tr>
-                                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"><?= $product["ProductNumber"]; ?></td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= $product["Model"]; ?></td>
-                                        
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= $product["Price"]; ?></td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                <img class="h-10 w-10 rounded-full" src="<?= $product["ProductMainImage"]; ?>" alt="Product image">
-                                            </td>
-                                            <td class="relative whitespace-nowrap pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                                <a href="update_product.php?ProductID=<?php echo $product['ProductID']; ?>" class="bg-green-500 text-white py-1 px-3 rounded hover:bg-green-600">Edit</a>
-                                                <a class="bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600" href="delete_product.php?ProductID=<?php echo $product['ProductID']; ?>"
-                                                onclick="return confirm('Are you sure you want to delete this product?');">
-                                                Delete
-                                                </a>
-                                            </td>
-                                        </tr>
-                                        <?php
-                                        endwhile;
-                                    else:
-                                        ?>
-                                        <tr><td colspan='13' class='py-3 px-6 text-center'>No products found</td></tr>
-                                        <?php
-                                    endif;
-
-                                    // Close connection
-                                    $conn->close();
-                                    ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+<body class="bg-gray-100 py-10">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:items-center">
+            <div class="sm:flex-auto">
+                <h1 class="text-base font-semibold leading-6 text-gray-900">Products</h1>
+                <p class="mt-2 text-sm text-gray-700">A list of all the products in the webshop. Including their attributes.</p>
+            </div>
+            <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                <a href="add_product.php" class="block rounded-md bg-amber-500 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm">Add product</a>
+            </div>
+        </div>
+        <div class="mt-8 flow-root">
+            <div class="mx-4 -my-2 overflow-x-auto sm:mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <div class="overflow-hidden border border-gray-300 sm:rounded-lg">
+                        <table class="min-w-full">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Number</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Name</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Price</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Image</th>
+                                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                        <span class="sr-only">Actions</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-300">
+                                <?php
+                                if ($products && $products->num_rows > 0):
+                                    while($product = $products->fetch_assoc()):
+                                        $productColors = $readProductCrud->getProductColors($product["ProductID"]);
+                                        $productSizes = $readProductCrud->getProductSizes($product["ProductID"]);
+                                        $categoryName = $readProductCrud->getCategoryName($product["CategoryID"]);
+                                        $brandName = $readProductCrud->getBrandName($product["BrandID"]);
+                                        $authorName = $readProductCrud->getAuthorName($product["AdminID"]);
+                                ?>
+                                <tr>
+                                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"><?= $product["ProductNumber"]; ?></td>
+                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= $product["Model"]; ?></td>
+                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= $product["Price"]; ?></td>
+                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                        <img class="h-10 w-10 rounded-full" src="<?= $product["ProductMainImage"]; ?>" alt="Product image">
+                                    </td>
+                                    <td class="relative whitespace-nowrap pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                        <a href="update_product.php?ProductID=<?php echo $product['ProductID']; ?>" class="bg-green-500 text-white py-1 px-3 rounded hover:bg-green-600">Edit</a>
+                                        <a class="bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600" href="delete_product.php?ProductID=<?php echo $product['ProductID']; ?>"
+                                        onclick="return confirm('Are you sure you want to delete this product?');">
+                                        Delete
+                                        </a>
+                                    </td>
+                                </tr>
+                                <?php
+                                    endwhile;
+                                else:
+                                ?>
+                                <tr><td colspan='5' class='py-3 px-6 text-center'>No products found</td></tr>
+                                <?php
+                                endif;
+                                ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-<script>
-
-    // Add event listener to the button
-    document.addEventListener("DOMContentLoaded", function () {
-        let showModalBtn = document.getElementById("showModalBtn");
-        if (showModalBtn) {
-            showModalBtn.addEventListener("click", loadAndShowModal);
-        }
-    });
-
-    // Display the selected file name in the input field
-   function displayFileName() {
-    var input = document.getElementById("ProductMainImage");
-    if (input.files && input.files[0]) {
-        var fileName = input.files[0].name;
-        document.getElementById("file-name").textContent = "Selected file: " + fileName;
-    }
-}
-</script>
 </body>
 </html>
-
