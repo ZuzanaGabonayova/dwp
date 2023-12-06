4d7d0a661c22a81509bfaf74ee7c36be80886638
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-06 12:49:33 +0100

    fixed redirect link

--- src/actions/handle_update_company_presentation.php
+++ src/actions/handle_update_company_presentation.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Update the company presentation
     if ($updateCrud->updateCompanyPresentation($description, $openingHours, $email, $phone, $street, $houseNumber, $postalCodeID)) {
      // Redirect to company.php after successful update
-        header('Location: company.php');
+        header('Location: ../views/company.php');
         exit();
     } else {
         echo "Error updating company presentation.";
