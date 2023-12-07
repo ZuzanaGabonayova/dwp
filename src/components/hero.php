08ef09c54a37f094f3e4f075cd301b908098bcaa
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-07 12:42:51 +0100

    update hero section

--- src/components/hero.php
+++ src/components/hero.php
@@ -23,6 +23,25 @@
             </ul>
         </div>
     </nav>
-    <div></div>
+    <div class="relative">
+        <div aria-hidden="true" class="absolute hidden h-full w-1/2 bg-gray-100 lg:block"></div>
+        <div class="relative bg-gray-200 lg:bg-transparent">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:grid lg:grid-cols-2 lg:px-8">
+                <div class="mx-auto max-w-2xl py-24 lg:max-w-none lg:py-64">
+                    <div class="lg:pr-16">
+                        <h1 class="font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">Focus on what matters</h1>
+                        <p class="mt-4 text-xl text-gray-600">All the charts, datepickers, and notifications in the world can't beat checking off some items on a paper card.</p>
+                        <div class="mt-6">
+                            <a href="#" class="inline-block rounded-md border border-transparent bg-orange-500 px-8 py-3 font-medium text-white">
+                                Shop Sneakers
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="h-48 w-full sm:h-64 lg:absolute lg:right-0 lg:top-0 lg:h-full lg:w-1/2">
+            <img src="../../assets/images/dwp_hero_sneaker.jpg" alt="" class="h-full w-full object-cover object-center">
+        </div>
+    </div>
 </div>
-<section></section>
