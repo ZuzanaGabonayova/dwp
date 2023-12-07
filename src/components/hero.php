10dfe7864c51dde0f551f795e23349658d14ca81
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-07 10:58:05 +0100

    update hero nav borders

--- src/components/hero.php
+++ src/components/hero.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col border-b border-gray-200 lg:border-0">
     <nav aria-label="Offers" class="order-last lg:order-first">
         <div class="mx-auto max-w-7xl lg:px-8">
-            <ul role="list" class="grid grid-cols-1 lg:grid-cols-3 divide-y lg:divide-x">
+            <ul role="list" class="grid grid-cols-1 lg:grid-cols-3 divide-y lg:divide-y-0 lg:divide-x">
                 <li class="flex flex-col">
                     <a href="#" class="relative flex flex-1 flex-col justify-center bg-white px-4 py-6 text-center">
                         <p class="text-sm text-gray-500">Download the app</p>
