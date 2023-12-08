9b9ffc9dfff94433953f32909ab701abbd2ad479
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-08 15:20:18 +0100

    removed env package

--- src/components/HomeCategories.php
+++ src/components/HomeCategories.php
@@ -19,7 +19,7 @@
             </div>
           <div class="group relative">
               <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                <img src="../../assets/images/dwp_category_card_men.jpg" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." class="h-full w-full object-cover object-center">
+                <img src="../../assets/images/dwp_category_card_men.jpg" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." class="h-full w-full object-cover object-center aspect-w-1 aspect-h-1 ">
               </div>
               <h3 class="mt-6 text-sm text-gray-500">
                 <a href="#" class="text-base font-semibold text-gray-900">
