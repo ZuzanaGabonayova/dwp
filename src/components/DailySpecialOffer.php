68756d27d2aea8f47c3988bc74ceca54c5371e67
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-08 16:49:58 +0100

    created backend layout file

--- src/components/DailySpecialOffer.php
+++ src/components/DailySpecialOffer.php
@@ -20,7 +20,7 @@
     <?php if ($productDetails): ?>
         <section aria-labelledby="daily-special-offer-heading" class="bg-white">
             <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
-                <div>
+                <div class="max-w-xl">
                     <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Daily Special Offer</h2>
                     <h3 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl"><?= htmlspecialchars($productDetails['Model']) ?></h3>
                     <p class="mt-4 text-gray-500">The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated steel divider separates active cards from new ones, or can be used to archive important task lists.</p>
--- src/config/db.php
+++ src/config/db.php
@@ -1,13 +1,11 @@
 <?php
 $servername = "142.93.169.155";
-$username = "phqmbyaurd"; // default XAMPP username
-$password = "5hw7pJkTr2"; // default XAMPP password
+$username = "phqmbyaurd"; 
+$password = "5hw7pJkTr2"; 
 $dbname = "phqmbyaurd";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
