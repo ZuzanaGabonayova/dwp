4579b85e7b4899e9f70683d6a9c4cd235c672718
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-08 17:28:52 +0100

    update backend.php

--- src/components/admin_content_wrapper.php
+++ src/components/admin_content_wrapper.php
@@ -1,7 +1,27 @@
-<main class="py-10">
+<div class="lg:pl-72">
+        <div
+          class="lg:hidden top-0 flex h-16 items-center border-b border-gray-200 bg-white px-4 shadow-sm"
+        >
+          <button id="hamburger-menu" class="lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+          </button>
+        </div>
+       <main class="py-10">
   <div class="px-4 sm:px-6 lg:px-8">
     <div id="content-wrapper" class="relative overflow-hidden border border-dashed border-gray-400 opacity-75 content-area">
       <?php include $content; ?>
     </div>
-  </div>
-</main>
+    </div>
+    </main>
+</div>
+</div>
