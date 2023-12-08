0720b827ad2ad0e0bc1e7ae9da58fcdd7841f188
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-08 17:25:25 +0100

    update backend.php

--- src/layouts/backend.php
+++ src/layouts/backend.php
@@ -3,15 +3,15 @@ include_once '../components/sidebar.php';
 ?>
 
 <div class="lg:pl-72">
-<?php
-include_once '../components/mobile_menu_backend.php';
-?>
-<main class="py-10">
-  <div class="px-4 sm:px-6 lg:px-8">
-    <div id="content-wrapper" class="relative overflow-hidden border border-dashed border-gray-400 opacity-75 content-area">
-      <?php include $content; ?>
+    <?php
+    include_once '../components/mobile_menu_backend.php';
+    ?>
+    <main class="py-10">
+    <div class="px-4 sm:px-6 lg:px-8">
+        <div id="content-wrapper" class="relative overflow-hidden border border-dashed border-gray-400 opacity-75 content-area">
+        <?php include $content; ?>
+        </div>
     </div>
-  </div>
-</main>
+    </main>
 </div>
 
