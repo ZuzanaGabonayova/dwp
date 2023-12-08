a0c41735644aafea874d25651a982022209e0d2f
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-08 16:53:55 +0100

    create admin.php

---
+++ src/views/admin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../../assets/css/output.css">
+</head>
+<body>
+    <?php
+
+    include_once '../layouts/backend.php';
+
+    ?>
+</body>
+</html>
