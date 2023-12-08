2e39ff7737f644fc63d53e542423b2fb941023b0
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-08 10:50:38 +0100

    update index link

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-require_once 'vendor/autoload.php';
+require_once './vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable('.');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 ?>
 
