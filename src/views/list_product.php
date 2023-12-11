10c3e70d0932862d474d5074fd59b5771eda2716
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-11 15:20:13 +0100

    Update product list heading

--- src/views/list_product.php
+++ src/views/list_product.php
@@ -23,7 +23,7 @@ $products = $readProductCrud->readProducts();
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
         <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
             <div class="flex items-center justify-between">
-                <h2 class="text-base font-semibold text-gray-900">Company Presentation</h2>
+                <h2 class="text-base font-semibold text-gray-900">Products</h2>
                 <a href="../views/add_product.php" class="flex items-center gap-x-1 rounded-md bg-blue-500 hover:bg-blue-700 text-white px-3 py-2 text-sm font-semibold shadow-sm">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 -ml-1.5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" />
