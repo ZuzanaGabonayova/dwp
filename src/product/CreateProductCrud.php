0252d06056ef81a1c8167dbcbeef2bf877499188
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-11 22:39:35 +0100

    Update file paths in CreateProductCrud.php

--- src/product/CreateProductCrud.php
+++ src/product/CreateProductCrud.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once '../config/db.php';
-require_once '../utils/uploadProductImage.php';
+require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../utils/uploadProductImage.php';
 
 class CreateProductCrud {
     private $conn;
