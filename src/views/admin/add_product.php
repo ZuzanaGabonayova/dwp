b11ce5e9a31141d78de2b0c2083613cbbc3ce2ac
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-11 22:37:21 +0100

    Fix require_once path in add_product.php

--- src/views/admin/add_product.php
+++ src/views/admin/add_product.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-require_once '../product/CreateProductCrud.php';
+require_once __DIR__ . '/../product/CreateProductCrud.php';
 
 $crud = new CreateProductCrud($conn);
 
