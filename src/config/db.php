841a11e7a251e72b5e0930f9aa594a8f6530a567
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-11 16:34:05 +0100

    Update database configuration

--- src/config/db.php
+++ src/config/db.php
@@ -1,17 +1,21 @@
 <?php
+use Dotenv\Dotenv;
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv = Dotenv::createImmutable('/home/master/applications/phqmbyaurd/public_html'); // Adjusted path to load .env from two directories back
 $dotenv->load();
 
-$servername = getenv('DB_HOST');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASS');
-$dbname = getenv('DB_NAME');
+$servername = $_ENV['DB_HOST'] ?? null;
+$username = $_ENV['DB_USER'] ?? null;
+$password = $_ENV['DB_PASS'] ?? null;
+$dbname = $_ENV['DB_NAME'] ?? null;
+
+var_dump($servername, $username, $password, $dbname);
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
