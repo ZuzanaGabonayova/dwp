50ee1ed7106521cbaef18aca9c4ccb9d64b4c8d7
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-11 22:03:08 +0100

    Fix file path in UpdateProductCrud.php

--- src/product/UpdateProductCrud.php
+++ src/product/UpdateProductCrud.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../config/db.php';
-require_once '../utils/uploadProductImage.php';
+require_once __DIR__ . '/../utils/uploadProductImage.php';
 
 class UpdateProductCrud {
     private $conn;
