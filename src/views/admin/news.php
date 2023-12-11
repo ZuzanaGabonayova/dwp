330852f78786e7a937bb154fd499338f5bd4abff
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-11 20:57:48 +0100

    Update CSS file path in news.php

--- src/views/admin/news.php
+++ src/views/admin/news.php
@@ -10,7 +10,7 @@ error_reporting(E_ALL);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>News Posts - Admin</title>
-    <link rel="stylesheet" href="<?php echo __DIR__; ?>/../../assets/css/output.css">
+    <link rel="stylesheet" href="../../../assets/css/output.css">
 </head>
 <body>
     <div>
