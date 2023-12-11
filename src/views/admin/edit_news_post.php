d8bd612f91f61779cc12211623d07105d6010600
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-11 23:53:04 +0100

    Add error reporting to edit_news_post.php

--- src/views/admin/edit_news_post.php
+++ src/views/admin/edit_news_post.php
@@ -1,5 +1,8 @@
 <?php
 session_start(); // Initialize the session for counting the cart items
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once '../config/db.php';
 require_once '../news/ReadNewsCrud.php';
