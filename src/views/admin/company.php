1a481b3bd261eedca3020f2c24568c179eec9d96
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-11 23:20:03 +0100

    Add and delete admin views and components

--- src/views/admin/company.php
+++ src/views/admin/company.php
@@ -2,6 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+require_once '../admin_authentication/loggedin.php';
 ?>
 
 <!DOCTYPE html>
@@ -9,15 +11,15 @@ error_reporting(E_ALL);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="../../assets/css/output.css">
+    <title>Company - Admin</title>
+    <link rel="stylesheet" href="../../../assets/css/output.css">
 </head>
 <body>
     <div>
         <?php
 
-        $content = '../views/list_product.php';
-        include_once '../layouts/backend.php';
+         $content = __DIR__ . '/../../components/admin/company.php';
+        include_once __DIR__ . '/../../layouts/backend.php';
 
         ?>
     </div>
