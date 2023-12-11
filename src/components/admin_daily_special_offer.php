859d0a194d0a8d343bf13b80af76543612ac0197
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-11 11:59:00 +0100

    Update form class in admin_daily_special_offer.php

--- src/components/admin_daily_special_offer.php
+++ src/components/admin_daily_special_offer.php
@@ -8,7 +8,7 @@ require_once '../product/ReadProductCrud.php';
 
 
 
-<form action="../actions/handle_special_offer.php" method="post">
+<form action="../actions/handle_special_offer.php" method="post" class="max-w-2xl">
     <label class="block mb-2 text-sm font-medium text-gray-900" for="CategoryID">Set daily special offer</label>
     <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" name="productId">
         <?php
