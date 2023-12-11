c5d105c2ee6ff52f84861f7b5ae1dc788ea02414
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-11 15:27:04 +0100

    Update dotenv path in db.php

--- src/config/db.php
+++ src/config/db.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 
 $servername = getenv('DB_HOST');
