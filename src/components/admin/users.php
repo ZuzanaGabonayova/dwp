9918210cbcc0ac1a3c3b061857dd02adeaed0b9e
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-12 12:09:50 +0100

    Update link to admin registration form

--- src/components/admin/users.php
+++ src/components/admin/users.php
@@ -14,9 +14,9 @@ $userData = $readCrud->readUsersCrud();
     <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
         <div class="flex items-center justify-between">
             <h2 class="text-base font-semibold text-gray-900">Users</h2>
-            <a href="../../views/admin/edit_company_presentation.php" class="flex items-center gap-x-1 rounded-md bg-blue-500 hover:bg-blue-700 text-white px-3 py-2 text-sm font-semibold shadow-sm">
+            <a href="../../views/admin/admin_registration_form.php" class="flex items-center gap-x-1 rounded-md bg-blue-500 hover:bg-blue-700 text-white px-3 py-2 text-sm font-semibold shadow-sm">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 -ml-1.5">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" />
                 </svg>
                 Add new user
             </a>
