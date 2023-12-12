473383ef254f9b9ff9685dedb1c214bce9417c63
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-12 20:20:17 +0100

    Add home content wrapper and daily special offer components

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<h1>hello</h1>
-
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
 
 <!DOCTYPE html>
@@ -10,18 +9,19 @@ ini_set('display_startup_errors', 1);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Shoes Webshop - Home</title>
+    <link rel="stylesheet" href="/assets/css/output.css">
 </head>
 <body>
-    <a href="./src/views/admin.php">Admin</a><br>
-    <a href="./src/views/list_product.php">Product list</a><br>
-    <a href="./src/views/news_post_form.php">Add news post</a><br>
-    <a href="./src/components/contact_form.php">Contact form</a><br>
-    <a href="./src/views/visitor_product_page.php">Visitor products</a><br>
-    <a href="./src/views/news.php">Visitor news</a><br>
-    <a href="./src/views/daily-special-offer.php">Daily special offer</a><br>
-    <a href="./src/views/checkout.php">Checkout</a><br>
-    <a href="./src/views/admin/admin_registration_form.php">Admin registration</a><br>
-    <a href="./src/views/admin/admin_login.php">Admin login</a><br>
+    <div>
+
+        <?php
+
+        $content = __DIR__ . 'src/components/frontend/home_content_wrapper.php';
+        include_once __DIR__ . '/src/layouts/frontend.php';
+
+        ?>
+
+    </div>
 </body>
 </html>
---
+++ src/components/frontend/home_content_wrapper.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once './home_hero';
+include_once './home_categories';
+include_once './home_daily_special_offer';
+
+?>
