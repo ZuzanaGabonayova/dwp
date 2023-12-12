0553f38f6953baf7f2e6ce03c855d8fce4211dd3
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-12 23:37:59 +0100

    link corrected

--- assets/js/admin_inactivity.js
+++ assets/js/admin_inactivity.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let inactivityTime = 60000; // 1 minute in milliseconds
+    let inactivityTime = 30000; // 30 seconds in milliseconds
     let timeout;
 
     function setActivity() {
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function logoutUser() {
         // AJAX call to update the server's last activity time using a separate PHP script
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", "../src/admin_authentication/update_activity.php", true); // Update_activity.php should handle only updating the last activity time
+        xhr.open("GET", "/src/admin_authentication/update_activity.php", true); // Update_activity.php should handle only updating the last activity time
         xhr.send();
     }
 
