08b4505c80869916b3da3d9ad0816f98e0596e8b
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-12 22:27:46 +0100

    created script for checking out admins activity on page to logout if inactive

---
+++ assets/js/admin_inactivity.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let inactivityTime = 60000; // 1 minute in milliseconds
+    let timeout;
+
+    function setActivity() {
+        clearTimeout(timeout);
+        timeout = setTimeout(logoutUser, inactivityTime);
+    }
+
+    function logoutUser() {
+        // AJAX call to update the server's last activity time using a separate PHP script
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "../../src/admin_authentication/update_activity.php", true); // Update_activity.php should handle only updating the last activity time
+        xhr.send();
+    }
+
+    // Event listeners to detect user activity
+    document.addEventListener("mousemove", setActivity);
+    document.addEventListener("mousedown", setActivity);
+    document.addEventListener("keypress", setActivity);
+
+    setActivity(); // Set initial activity on page load
+});
