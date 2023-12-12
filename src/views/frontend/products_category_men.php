bc257aa3e9ca3b613013c18b62130471585283db
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-12 13:16:33 +0100

    Fix bug in login functionality

---
+++ src/views/frontend/products_category_men.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Men Products</title>
+    <link rel="stylesheet" href="../../../assets/css/output.css">
+</head>
+<body>
+    <main class="mx-auto max-w-2xl px-4 lg:max-w-7xl lg:px-8">
+        <div class="border-b border-gray-200 pb-10">
+            <h1 class="text-4xl font-bold tracking-tight text-gray-900">Men Category</h1>
+            <p class="mt-4 text-base text-gray-500">Explore our wide range of men's sneakers. From classic designs to the latest trends, we have it all. Perfect for sports, casual outings, or just making a fashion statement.</p>
+        </div>
+        <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+      <div class="group relative">
+        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+          <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Basic Tee
+              </a>
+            </h3>
+            <p class="mt-1 text-sm text-gray-500">Black</p>
+          </div>
+          <p class="text-sm font-medium text-gray-900">$35</p>
+        </div>
+      </div>
+
+      <!-- More products... -->
+    </div>
+    </main>
+</body>
+</html>
