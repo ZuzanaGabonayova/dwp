78905aac814d775b165442a54e64c57035a25298
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-12 19:22:37 +0100

    session_start removed from news

--- src/components/frontend/news.php
+++ src/components/frontend/news.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 
-session_start(); // Initialize the session for counting the cart items
+//session_start(); // Initialize the session for counting the cart items
 
 require_once '../../config/db.php'; 
 require_once '../../news/ReadNewsCrud.php'; 
