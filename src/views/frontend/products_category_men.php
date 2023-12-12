6a985f8556095098efca070a7ffe67e1c9bf6eb6
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-12 13:35:06 +0100

    Update image source path in products_category_men.php

--- src/views/frontend/products_category_men.php
+++ src/views/frontend/products_category_men.php
@@ -28,12 +28,12 @@ $menProducts = $readProductCrud->readProductsByCategory('men');
                 <?php while ($product = $menProducts->fetch_assoc()) : ?>
                     <div class="group relative">
                         <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                            <img src="<?= htmlspecialchars($product['ProductMainImage']) ?>" alt="<?= htmlspecialchars($product['Model']) ?>" class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                            <img src="../<?= htmlspecialchars($product['ProductMainImage']) ?>" alt="<?= htmlspecialchars($product['Model']) ?>" class="h-full w-full object-cover object-center lg:h-full lg:w-full">
                         </div>
                         <div class="mt-4 flex justify-between">
                             <div>
                                 <h3 class="text-sm text-gray-700">
-                                    <a href="#">
+                                    <a href="single_product.php?ProductID=<?php echo $product['ProductID']; ?>">
                                         <span aria-hidden="true" class="absolute inset-0"></span>
                                         <?= htmlspecialchars($product['Model']) ?>
                                     </a>
