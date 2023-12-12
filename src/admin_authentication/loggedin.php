dc2e884c5873053f63579be67bfc2019d3935099
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-12 15:42:59 +0100

    implemented session timeout functionality on admin pages

--- src/admin_authentication/loggedin.php
+++ src/admin_authentication/loggedin.php
@@ -1,6 +1,33 @@
 <?php
 session_start();
 
+// Function to update last activity time
+function updateLastActivityTime() {
+    $_SESSION['last_activity_time'] = time(); // Update the last activity time to the current time
+}
+
+// Function to check for session timeout and log out if needed
+function checkSessionTimeout() {
+    $timeout = 15; // 15 minutes in seconds
+
+    if (isset($_SESSION['last_activity_time']) && (time() - $_SESSION['last_activity_time']) > $timeout) {
+        // If the difference between current time and last activity time exceeds the timeout, logout
+        session_unset(); // Unset all session variables
+        session_destroy(); // Destroy the session
+        header("Location: ../views/admin/admin_login.php"); // Redirect to the login page
+        exit();
+    } else {
+        // Update last activity time as the user is active
+        updateLastActivityTime();
+    }
+}
+
+// Call the function to update last activity time
+updateLastActivityTime();
+
+// Call the function to check for session timeout
+checkSessionTimeout();
+
 // Check if the admin is not logged in
 if (!isset($_SESSION['admin_username'])) {
     header("Location: ../views/admin/admin_login.php"); // Redirect to the admin login page
