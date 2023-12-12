340db9a213c54ae781cdcc320048ed732830acd3
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-12 09:09:51 +0100

    sidebar admin avatar

--- src/components/sidebar.php
+++ src/components/sidebar.php
@@ -1,6 +1,18 @@
 <?php 
 include_once __DIR__ . '/../utils/url_helpers.php';
-require_once '../../admin_authentication/loggedin.php';
+require_once __DIR__ . '../../admin_authentication/loggedin.php';
+
+// Get the logged-in admin's username from the session
+$admin_username = $_SESSION['admin_username'] ?? '';
+
+// Function to display the admin's avatar
+function displayAdminAvatar() {
+    return '<div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
+                </svg>
+            </div>';
+}
 ?>
 
 <div
