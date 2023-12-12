ef8b3ea241addfdb30203eff86cc634a51ba6ac9
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-12 20:24:06 +0100

    Fix file paths in home_content_wrapper.php

--- src/components/frontend/home_content_wrapper.php
+++ src/components/frontend/home_content_wrapper.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once './home_hero.php';
-include_once './home_categories.php';
-include_once './home_daily_special_offer.php';
+include_once '/home_hero.php';
+include_once '/home_categories.php';
+include_once '/home_daily_special_offer.php';
 
 ?>
