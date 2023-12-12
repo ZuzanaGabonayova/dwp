511a03a6eac7d322b4650d94954f3f9604e7cfca
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-12 17:40:44 +0100

    echo baseurl removed

--- src/views/admin/admin_login.php
+++ src/views/admin/admin_login.php
@@ -12,7 +12,7 @@
     </form> -->
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <a href="<?php echo baseUrl(); ?>index.php">
+        <a href="../../../index.php">
         <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
         </a>
         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
