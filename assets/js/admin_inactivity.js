8eaf4f81ea526c9ded58e61f7ed9e8e79b8c3582
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-12 23:49:37 +0100

    admin inactivity logout function

--- assets/js/admin_inactivity.js
+++ assets/js/admin_inactivity.js
@@ -8,9 +8,19 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function logoutUser() {
+        console.log("Logging out user due to inactivity...");
         // AJAX call to update the server's last activity time using a separate PHP script
         let xhr = new XMLHttpRequest();
         xhr.open("GET", "/src/admin_authentication/update_activity.php", true); // Update_activity.php should handle only updating the last activity time
+        
+        xhr.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                console.log("Activity updated successfully.");
+            } else if (this.readyState === 4 && this.status !== 200) {
+                console.log("Error updating activity:", this.status, this.statusText);
+            }
+        };
+        
         xhr.send();
     }
 
