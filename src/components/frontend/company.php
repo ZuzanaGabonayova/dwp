f9fc243e1c064fbca63f78e59062e564b12d0bd7
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-12 18:39:28 +0100

    Add company details to company.php

--- src/components/frontend/company.php
+++ src/components/frontend/company.php
@@ -2,6 +2,12 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+require_once __DIR__ . '../../../config/db.php';
+require_once __DIR__ . '../../../company/ReadCompanyCrud.php';
+
+$crud = new ReadCompanyCrud($conn);
+$companyDetails = $crud->readCompanyPresentation();
 ?>
 
 <div class="relative isolate bg-white">
@@ -23,7 +29,7 @@ error_reporting(E_ALL);
           </svg>
         </div>
         <h2 class="text-3xl font-bold tracking-tight text-gray-900">About us</h2>
-        <p class="mt-6 text-lg leading-8 text-gray-600">Description</p>
+        <p class="mt-6 text-lg leading-8 text-gray-600"><?php echo $companyDetails['DescriptionOfCompany']; ?></p>
         <dl class="mt-10 space-y-4 text-base leading-7 text-gray-600">
           <div class="flex gap-x-4">
             <dt class="flex-none">
@@ -32,7 +38,7 @@ error_reporting(E_ALL);
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
               </svg>
             </dt>
-            <dd>545 Mavis Island<br>Chicago, IL 99191</dd>
+            <dd><?php echo $companyDetails['Street'], ', ', $companyDetails['HouseNumber'], '<br>', $companyDetails['PostalCodeID']; ?></dd>
           </div>
           <div class="flex gap-x-4">
             <dt class="flex-none">
@@ -41,7 +47,12 @@ error_reporting(E_ALL);
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
               </svg>
             </dt>
-            <dd><a class="hover:text-gray-900" href="tel:+1 (555) 234-5678">+1 (555) 234-5678</a></dd>
+            <!-- Phone -->
+            <dd>
+                <a class="hover:text-gray-900" href="tel:+45 <?php echo $companyDetails['Phone']; ?>">
+                    <?php echo $companyDetails['Phone']; ?>
+                </a>
+            </dd>
           </div>
           <div class="flex gap-x-4">
             <dt class="flex-none">
@@ -50,7 +61,12 @@ error_reporting(E_ALL);
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
               </svg>
             </dt>
-            <dd><a class="hover:text-gray-900" href="mailto:hello@example.com">hello@example.com</a></dd>
+            <!-- Email -->
+            <dd>
+                <a class="hover:text-gray-900" href="mailto:<?php echo $companyDetails['Email']; ?>">
+                <?php echo $companyDetails['Email']; ?>
+                </a>
+            </dd>
           </div>
         </dl>
       </div>
