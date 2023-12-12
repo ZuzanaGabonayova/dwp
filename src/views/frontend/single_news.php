bc0f33393822612a57eb1b9cf14da509daaadaad
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-12 19:39:02 +0100

    Fix require_once path in single_news.php

--- src/views/frontend/single_news.php
+++ src/views/frontend/single_news.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../../config/db.php';
-require_once __DIR__ . '/../../product/ReadNewsCrud.php';
+require_once __DIR__ . '/../../news/ReadNewsCrud.php';
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
