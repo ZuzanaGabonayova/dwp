300a7968789a2e8c46cce585d826ffdfed94a3d7
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-12 15:32:27 +0100

    links

--- src/components/HomeCategories.php
+++ src/components/HomeCategories.php
@@ -11,7 +11,7 @@
                 <img src="../../assets/images/dwp_category_card_women.jpg" alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." class="h-full w-full object-cover object-center">
               </div>
               <h3 class="mt-6 text-sm text-gray-500">
-                <a href="#" class="text-base font-semibold text-gray-900">
+                <a href="../views/frontend/products_category_women.php" class="text-base font-semibold text-gray-900">
                   <span class="absolute inset-0"></span>
                   Women
                 </a>
@@ -22,7 +22,7 @@
                 <img src="../../assets/images/dwp_category_card_men.jpg" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." class="h-full w-full object-cover object-center">
               </div>
               <h3 class="mt-6 text-sm text-gray-500">
-                <a href="#" class="text-base font-semibold text-gray-900">
+                <a href="../views/frontend/products_category_men.php" class="text-base font-semibold text-gray-900">
                   <span class="absolute inset-0"></span>
                   Men
                 </a>
@@ -33,7 +33,7 @@
                 <img src="../../assets/images/dwp_category_card_kids.jpg" alt="Collection of four insulated travel bottles on wooden shelf." class="h-full w-full object-cover object-center">
               </div>
               <h3 class="mt-6 text-sm text-gray-500">
-                <a href="#" class="text-base font-semibold text-gray-900">
+                <a href="../views/frontend/products_category_kids.php" class="text-base font-semibold text-gray-900">
                   <span class="absolute inset-0"></span>
                   Kids
                 </a>
