54b8b81c252e0916f63e6ad9419c0acfe1e21db8
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-12 23:30:33 +0100

    log out admin if inactive functionality

--- src/admin_authentication/update_activity.php
+++ src/admin_authentication/update_activity.php
@@ -6,6 +6,25 @@ function updateLastActivityTime() {
     $_SESSION['last_activity_time'] = time(); // Update the last activity time to the current time
 }
 
+// Function to check for session timeout and log out if needed
+function checkSessionTimeout() {
+    $timeout = 30; // 30 seconds
+
+    if (isset($_SESSION['last_activity_time']) && (time() - $_SESSION['last_activity_time']) > $timeout) {
+        // If the difference between current time and last activity time exceeds the timeout, logout
+        session_unset(); // Unset all session variables
+        session_destroy(); // Destroy the session
+        echo json_encode(["status" => "logged_out"]); // Signal that the user is logged out
+        exit();
+    } else {
+        // Update last activity time as the user is active
+        updateLastActivityTime();
+    }
+}
+
 // Call the function to update last activity time
 updateLastActivityTime();
+
+// Call the function to check for session timeout
+checkSessionTimeout();
 ?>
