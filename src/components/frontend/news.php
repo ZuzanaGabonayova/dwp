ea1ad525f1b3a16dc2d417b581a9540b136f7a74
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-12 19:17:19 +0100

    Fix image source path in news.php

--- src/components/frontend/news.php
+++ src/components/frontend/news.php
@@ -29,7 +29,7 @@ $newsPosts = $readNewsCrud->readAllNewsPosts();
             <?php while ($newsPost = $newsPosts->fetch_assoc()): ?>      
       <article class="flex flex-col items-start justify-between">
         <div class="relative w-full">
-          <img src="<?php echo htmlspecialchars($newsPost['image']); ?>" alt="<?php echo htmlspecialchars($newsPost['image_alt']); ?>" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">
+          <img src="../<?php echo htmlspecialchars($newsPost['image']); ?>" alt="<?php echo htmlspecialchars($newsPost['image_alt']); ?>" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">
           <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
         </div>
         <div class="max-w-xl">
