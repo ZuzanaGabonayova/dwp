abc96d503eb9cbf30bd40ace79bfc95a7b6d0784
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-12 19:00:00 +0100

    Remove background pattern from company.php

--- src/components/frontend/company.php
+++ src/components/frontend/company.php
@@ -15,18 +15,6 @@ $companyDetails = $crud->readCompanyPresentation();
     <div class="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
       <div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
         <div class="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2">
-          <svg class="absolute inset-0 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]" aria-hidden="true">
-            <defs>
-              <pattern id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527" width="200" height="200" x="100%" y="-1" patternUnits="userSpaceOnUse">
-                <path d="M130 200V.5M.5 .5H200" fill="none" />
-              </pattern>
-            </defs>
-            <rect width="100%" height="100%" stroke-width="0" fill="white" />
-            <svg x="100%" y="-1" class="overflow-visible fill-gray-50">
-              <path d="M-470.5 0h201v201h-201Z" stroke-width="0" />
-            </svg>
-            <rect width="100%" height="100%" stroke-width="0" fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
-          </svg>
         </div>
         <h2 class="text-3xl font-bold tracking-tight text-gray-900">About us</h2>
         <p class="mt-6 text-lg leading-8 text-gray-600"><?php echo $companyDetails['DescriptionOfCompany']; ?></p>
