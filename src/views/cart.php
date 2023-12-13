8ad31e1890f997827e6c8c9bb33eec75bb10a98d
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-13 17:00:07 +0100

    Add error reporting to webhook and cart views

--- src/views/cart.php
+++ src/views/cart.php
@@ -1,6 +1,8 @@
 <?php
-error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 session_start();
 require '../config/db.php'; // Include the database configuration
