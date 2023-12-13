0d0bd9016eeaea248a66093f08b04f4ff8c945a6
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-13 22:50:29 +0100

    Remove News and Company links from navigation bar

--- src/components/frontend/navigationbar.php
+++ src/components/frontend/navigationbar.php
@@ -48,8 +48,6 @@ require_once __DIR__ . '/../../utils/url_helpers.php';
       <a href="<?php echo baseUrl(); ?>src/views/frontend/products_category_women.php" class="text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50">Women</a>
       <a href="<?php echo baseUrl(); ?>src/views/frontend/products_category_men.php" class="text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50">Men</a>
       <a href="<?php echo baseUrl(); ?>src/views/frontend/products_category_kids.php" class="text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50">Kids</a>
-      <a href="<?php echo baseUrl(); ?>src/views/frontend/news.php" class="text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50">News</a>
-      <a href="<?php echo baseUrl(); ?>src/views/frontend/company.php" class="text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50">Company</a>
     </div>
     <div class="hidden lg:flex lg:flex-1 lg:justify-end items-center">
         <div class="flex lg:ml-6">
@@ -103,8 +101,6 @@ require_once __DIR__ . '/../../utils/url_helpers.php';
             <a href="<?php echo baseUrl(); ?>src/views/frontend/products_category_women.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Women</a>
             <a href="<?php echo baseUrl(); ?>src/views/frontend/products_category_men.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Men</a>
             <a href="<?php echo baseUrl(); ?>src/views/frontend/products_category_kids.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Kids</a>
-            <a href="<?php echo baseUrl(); ?>src/views/frontend/news.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">News</a>
-            <a href="<?php echo baseUrl(); ?>src/views/frontend/company.php" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
           </div>
           <div class="py-6">
             <a href="<?php echo baseUrl(); ?>src/views/admin/admin_login.php" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
