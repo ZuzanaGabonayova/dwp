c3388473f8f82d6822d7c0049722f2aa019104a5
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-13 19:08:46 +0100

    single_page.php path links corrected

--- src/components/frontend/products_category_kids.php
+++ src/components/frontend/products_category_kids.php
@@ -24,7 +24,7 @@ $kidsProducts = $readProductCrud->readProductsByCategory('kids');
                         <div class="mt-4 flex justify-between">
                             <div>
                                 <h3 class="text-sm text-gray-700">
-                                    <a href="<?php echo baseUrl(); ?>src/views/single_product.php?ProductID=<?php echo $product['ProductID']; ?>">
+                                    <a href="<?php echo baseUrl(); ?>src/views/frontend/single_product.php?ProductID=<?php echo $product['ProductID']; ?>">
                                         <span aria-hidden="true" class="absolute inset-0"></span>
                                         <?= htmlspecialchars($product['Model']) ?>
                                     </a>
