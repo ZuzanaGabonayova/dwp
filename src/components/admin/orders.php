09339811765aaf60a118a26aa2f2a744dd0cd313
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-13 19:58:11 +0100

    Refactor orders.php to display products instead of orders

--- src/components/admin/orders.php
+++ src/components/admin/orders.php
@@ -1,12 +1,12 @@
 <?php
 require_once __DIR__ . '/../../config/db.php';
-require_once __DIR__ . '/../../orders/ReadOrders.php';
+require_once __DIR__ . '/../../product/ReadProductCrud.php';
 require_once __DIR__ . '/../../utils/url_helpers.php';
 
 
 
-$readOrders = new ReadOrders($conn);
-$orders = $readOrders->readOrders();
+$readProductCrud = new ReadProductCrud($conn);
+$products = $readProductCrud->readProducts();
 
 
 ?>
@@ -14,7 +14,7 @@ $orders = $readOrders->readOrders();
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
         <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
             <div class="flex items-center justify-between">
-                <h2 class="text-base font-semibold text-gray-900">Orders</h2>
+                <h2 class="text-base font-semibold text-gray-900">Products</h2>
                 <a href="<?php echo baseUrl(); ?>src/views/admin/add_product.php" class="flex items-center gap-x-1 rounded-md bg-blue-500 hover:bg-blue-700 text-white px-3 py-2 text-sm font-semibold shadow-sm">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 -ml-1.5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" />
@@ -28,30 +28,43 @@ $orders = $readOrders->readOrders();
                 <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                     <div class="overflow-hidden border border-gray-300 sm:rounded-lg">
                         <table class="min-w-full">
+                            <!-- Table Headers -->
                             <thead class="bg-gray-50">
                                 <tr>
-                                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Order ID</th>
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Order ID</th>
                                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Customer Name</th>
                                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Customer Email</th>
                                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Products</th>
-                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Quantity</th>
-                                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                                        <span class="sr-only">Actions</span>
-                                    </th>
+                                    <!-- Add more headers if necessary -->
                                 </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-300">
-                               
-                                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">Order ID</td>
-                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Echo Customer Name</td>
-                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Echo Customer Email</td>
-                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Echo Products</td>
-                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Echo Quantity</td>
-                                    <td class="relative whitespace-nowrap pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                        <a href="">Create invoice</a>
-                                    </td>
-                                </tr>
-                                
+                                <?php
+                                $currentOrderId = null;
+                                while ($row = $ordersResult->fetch_assoc()) {
+                                    if ($row['id'] != $currentOrderId) {
+                                        if ($currentOrderId != null) {
+                                            // Close the products cell and the previous row
+                                            echo "</td></tr>";
+                                        }
+                                        // Start a new row
+                                        ?>
+                                        <tr>
+                                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"><?= $row['id']; ?></td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= htmlspecialchars($row['customer_name']); ?></td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><?= htmlspecialchars($row['customer_email']); ?></td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                        <?php
+                                        $currentOrderId = $row['id'];
+                                    }
+                                    // Display products in the same cell
+                                    echo htmlspecialchars($row['product_name']) . " - Quantity: " . htmlspecialchars($row['quantity']) . "<br>";
+                                }
+                                if ($currentOrderId != null) {
+                                    // Close the last row
+                                    echo "</td></tr>";
+                                }
+                                ?>
                             </tbody>
                         </table>
                     </div>
