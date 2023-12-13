afd38f6428ab79e856ddfac4379df54b6924a92d
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-13 18:58:10 +0100

    Refactor webhook.php to handle checkout session completed event and insert order details into the database

--- src/actions/webhook.php
+++ src/actions/webhook.php
@@ -7,71 +7,72 @@ require_once __DIR__ . '../../../vendor/autoload.php';
 require_once __DIR__ . '../../config/db.php';
 
 // Stripe configuration
-$stripe = new \Stripe\StripeClient('sk_test_51OMqZxD7CQBEfsgzCUQ19XaHyqwJHTK9ejG5IjlGs4CaQUpBPSP8M4no8rgXkzfSm5DU0LIxUneFODPiblzB8lMQ0000soVBL9'); // Your secret key
-$endpoint_secret = 'whsec_Tfe8ILTXdbZ80jc9UwTIAeV9LJWJFozK'; // Your endpoint secret
+$stripe = new \Stripe\StripeClient('sk_test_51OMqZxD7CQBEfsgzCUQ19XaHyqwJHTK9ejG5IjlGs4CaQUpBPSP8M4no8rgXkzfSm5DU0LIxUneFODPiblzB8lMQ0000soVBL9');
+$endpoint_secret = 'whsec_Tfe8ILTXdbZ80jc9UwTIAeV9LJWJFozK';
 
 $payload = @file_get_contents('php://input');
 $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
 $event = null;
 
 try {
-  $event = \Stripe\Webhook::constructEvent($payload, $sig_header, $endpoint_secret);
+    $event = \Stripe\Webhook::constructEvent($payload, $sig_header, $endpoint_secret);
 } catch (\UnexpectedValueException $e) {
-  http_response_code(400); // Invalid payload
-  exit();
+    http_response_code(400);
+    exit();
 } catch (\Stripe\Exception\SignatureVerificationException $e) {
-  http_response_code(400); // Invalid signature
-  exit();
+    http_response_code(400);
+    exit();
 }
-
-
 
 // Handle the event
 switch ($event->type) {
-  case 'checkout.session.completed':
-    $session = $event->data->object;
+    case 'checkout.session.completed':
+        $session = $event->data->object;
 
-    // Check if customer details and email are set
-    $customerEmail = isset($session->customer_details->email) ? $session->customer_details->email : null;
+        $customerEmail = isset($session->customer_details->email) ? $session->customer_details->email : null;
+        $paymentMethodTypes = json_encode($session->payment_method_types);
+        $shippingAddress = json_encode($session->shipping_details);
 
-    // Serialize payment method types and shipping address
-    $paymentMethodTypes = json_encode($session->payment_method_types);
-    $shippingAddress = json_encode($session->shipping_details);
+        $stmt = $conn->prepare("INSERT INTO orders (session_id, payment_intent_id, amount_total, currency, customer_id, customer_email, payment_status, customer_name, customer_phone, shipping_address) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-    // Prepare an insert statement
-    $stmt = $conn->prepare("INSERT INTO orders (session_id, payment_intent_id, amount_total, currency, customer_id, customer_email, payment_status, customer_name, customer_phone, shipping_address) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $sessionId = $session->id;
+        $paymentIntentId = $session->payment_intent;
+        $amountTotal = $session->amount_total;
+        $currency = $session->currency;
+        $customerId = $session->customer;
+        $paymentStatus = $session->payment_status;
+        $customerName = $session->customer_details->name;
+        $customerPhone = $session->customer_details->phone;
 
+        $stmt->bind_param("ssisssssss", $sessionId, $paymentIntentId, $amountTotal, $currency, $customerId, $customerEmail, $paymentStatus, $customerName, $customerPhone, $shippingAddress);
 
-    // Extracting data from the session
-    $sessionId = $session->id;
-    $paymentIntentId = $session->payment_intent;
-    $amountTotal = $session->amount_total;
-    $currency = $session->currency;
-    $customerId = $session->customer; // Can be null
-    $paymentStatus = $session->payment_status;
-    $customerName = $session->customer_details->name;
-    $customerPhone = $session->customer_details->phone;
+        if ($stmt->execute()) {
+            echo "New record created successfully";
+            $order_id = $conn->insert_id;
 
+            // Fetch line items from Stripe
+            $lineItems = $stripe->checkout->sessions->allLineItems($sessionId, ['limit' => 5]);
+            foreach ($lineItems->data as $item) {
+                $productName = $item->description;
+                $quantity = $item->quantity;
 
-    // Bind variables to the prepared statement as parameters
-    $stmt->bind_param("ssisssssss", $sessionId, $paymentIntentId, $amountTotal, $currency, $customerId, $customerEmail, $paymentStatus, $customerName, $customerPhone, $shippingAddress);
+                $stmtLineItem = $conn->prepare("INSERT INTO order_line_items (order_id, product_name, quantity) VALUES (?, ?, ?)");
+                $stmtLineItem->bind_param("isi", $order_id, $productName, $quantity);
 
-    // Execute the query
-    if ($stmt->execute()) {
-      echo "New record created successfully";
-    } else {
-      echo "Error: " . $stmt->error;
-    }
+                if (!$stmtLineItem->execute()) {
+                    echo "Error inserting line item: " . $stmtLineItem->error;
+                }
+                $stmtLineItem->close();
+            }
+        } else {
+            echo "Error: " . $stmt->error;
+        }
 
-    // Close statement
-    $stmt->close();
-    break;
-  // ... [Other cases]
+        $stmt->close();
+        break;
+    // ... [Other cases]
 }
 
-
-// Close connection
 $conn->close();
-
 http_response_code(200);
 ?>
