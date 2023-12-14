43d78f610714762c857db989074efe1901b02133
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-14 21:53:16 +0100

    created data file for inserting into database

---
+++ data/data.sql
@@ -0,0 +1,188 @@
+INSERT INTO contacts (id, name, email, message, submitted_at) VALUES
+(1,	'Lasz0167',	'lasz0167@easv365.dk',	'hello',	'2023-11-07 10:28:22'),
+(2,	'laszlo',	'lasz0167@easv365.dk',	'new',	'2023-11-07 10:45:31'),
+(3,	'John',	'laszlo.vitkai@gmail.com',	'Hello',	'2023-11-07 10:53:39'),
+(4,	'Zuzana Gabonayova',	'gabonayova.zuzka@gmail.com',	'test',	'2023-11-07 23:17:05'),
+(5,	'kim',	'kim@kim.kim',	'jojojojojo',	'2023-11-18 15:23:01'),
+(6,	'kim',	'kim@kim.kim',	'jojojojojo',	'2023-11-18 15:23:26'),
+(7,	'tesst',	'vitkai.laca1@gmail.com',	'tst',	'2023-12-12 16:31:52'),
+(8,	'Zuzana Gabonayova',	'gabonayova.zuzka@gmail.com',	'testing recaptcha',	'2023-12-12 19:06:40');
+
+INSERT INTO news_posts (id, title, short_description, content, image, image_alt, created_at, updated_at) VALUES
+(1,	'Revolutionizing Comfort: New Shoe Tech Takes Stride in Footwear Industry',	'A breakthrough in shoe technology promises to redefine comfort and support for footwear enthusiasts.',	'In a groundbreaking development, footwear technology has taken a giant leap forward with the introduction of a revolutionary shoe design. Combining innovation with comfort, this latest creation is set to transform the way we perceive and experience shoes.\r\n\r\nEngineered with cutting-edge materials and ergonomic design principles, these shoes prioritize not only style but also the health and well-being of your feet. The incorporation of state-of-the-art cushioning and support mechanisms ensures unparalleled comfort, making walking or running an effortless and enjoyable experience.\r\n\r\nMoreover, the design caters to various lifestyles, seamlessly transitioning from casual wear to athletic performance. Whether youre hitting the streets or engaging in high-intensity workouts, these shoes promise to deliver comfort and support throughout.\r\n\r\nThis breakthrough in footwear technology comes as a game-changer in an industry constantly evolving to meet the needs of consumers. The fusion of style, functionality, and comfort in this latest shoe design marks a milestone in the pursuit of footwear excellence.',	'../../assets/images/news_images/news_65787633712c6.webp',	'tesla-revolution',	'2023-12-12 15:03:15',	'2023-12-12 15:03:15'),
+(2,	'Nike Unveils Futuristic \'AirStride\' Sneakers: The Next Evolution in Footwear Tech',	'Nike introduces \'AirStride,\' a cutting-edge shoe line revolutionizing performance and style in athletic footwear.',	'Nike has once again pushed the ssddsboundaries of innovation with the launch of their latest footwear sensation, \'AirStride.\' This futuristic line of sneakers represents a quantum leap in athletic shoe design, merging advanced technology with unparalleled comfort and style.\r\n\r\nAt the heart of \'AirStride\' lies Nike\'s groundbreaking cushioning system, engineered to provide maximum impact absorption and energy return. This technology not only enhances performance but also ensures a comfortable experience for wearers, whether sprinting on the track or navigating city streets.\r\n\r\nThe sleek and aerodynamic design of the \'AirStride\' series isn\'t just about aestheticsâ€”it\'s a testament to Nike\'s commitment to pushing the envelope in both style and functionality. The incorporation of cutting-edge materials ensures durability, flexibility, and breathability, setting a new benchmark in athletic shoe construction.\r\n\r\nMoreover, \'AirStride\' isn\'t just a shoe; it\'s a statement. Nike\'s commitment to sustainability shines through in this line, with a focus on eco-friendly manufacturing processes and materials, aligning with the company\'s dedication to reducing its environmental impact.\r\n\r\nThe \'AirStride\' collection isn\'t just a set of sneakers; it\'s a paradigm shift in the athletic footwear landscape. With its fusion of performance-enhancing technology, style, and sustainability, Nike once again asserts its position as an industry leader.',	'../../assets/images/news_images/news_6578a3ec372c5.webp',	'nike-AirStride',	'2023-12-12 18:18:20',	'2023-12-14 14:36:01'),
+(3,	'Fressssh Kicks Alert! Explore New Arrivals at Sneakers Today!',	'The hottest sneakers of the season!',	'E1xcitement is in the air as Sneakers drops the hottest sneakers of the season! Dive into a collection of exclusive releases and trendsetting styles that promise to elevate your sneaker game. Limited quantities, maximum style ï¿½ snag your pair now and step into a world of unmatched footwear fashion. Don\t miss out, visit Sneakers online or in-store today! #SneakerStyle #FreshKicks #NewArrivals',	'../../assets/images/news_images/news_65706cde07d96.jpg',	'news3',	'2023-11-12 11:28:48',	'2023-12-06 12:46:09');
+
+INSERT INTO PostalCode (PostalCodeID, PostalCode, City) VALUES
+(1,	'592 13',	'Bohdalov'),
+(2,	'458 10',	'Yanglang'),
+(3,	'56030',	'San Juan'),
+(4,	'453150',	'Karmaskaly'),
+(5,	'30404 CEDEX',	'Villeneuve-lès-Avignon'),
+(6,	'12345',	'Banjarwaru'),
+(7,	'458 107',	'Yangying Chengguanzhen'),
+(8,	'41231',	'Uurainen'),
+(9,	'83514 CEDEX',	'La Seyne-sur-Mer'),
+(10,	'H9J',	'Stonewall'),
+(11,	'34629',	'Clearwater'),
+(12,	'1248',	'Sumusţā as Sulţānī'),
+(13,	'31110',	'Nang Rong'),
+(14,	'67975 CEDEX 9',	'Strasbourg'),
+(15,	'1024',	'Monjarás');
+
+INSERT INTO PresentationOfCompany (DescriptionOfCompany, OpeningHours, Email, Phone, Street, HouseNumber, PostalCodeID) VALUES
+('A dynamic and innovative company providing cutting-edge solutions.',	'Mon-Fri 9:00 AM to 6:00 PM',	'info@company.com',	'123-456-7890',	'Main Street',	'592 13',	2);
+
+INSERT INTO Size (SizeID, Size) VALUES
+(1,	35),
+(2,	36),
+(3,	37),
+(4,	37.5),
+(5,	38),
+(6,	38.5),
+(7,	39),
+(8,	39.5),
+(9,	40),
+(10,	40.5),
+(11,	41),
+(12,	41.5),
+(13,	42),
+(14,	42.5),
+(15,	43),
+(16,	43.5),
+(17,	44),
+(18,	44.5),
+(19,	45),
+(20,	45.5),
+(21,	46),
+(22,	46.5),
+(23,	47);
+
+INSERT INTO ProductCategory (CategoryID, CategoryName) VALUES
+(1,	'men'),
+(2,	'women'),
+(3,	'kids');
+
+INSERT INTO ProductBrand (BrandID, BrandName) VALUES
+(1,	'Nike'),
+(2,	'Adidas'),
+(3,	'Puma'),
+(4,	'Reebok'),
+(5,	'New Balance'),
+(6,	'Vans'),
+(7,	'Converse'),
+(8,	'Under Armour'),
+(9,	'Tommy Hilfiger'),
+(10, 'Jordan');
+
+INSERT INTO Color (ColorID, ColorName) VALUES
+(4,	'black'),
+(5,	'blue'),
+(21,'blue violet'),
+(11,'brown'),
+(13,'gold'),
+(12,'gray'),
+(6,	'green'),
+(17,'indigo'),
+(20,'khaki'),
+(16,'lime green'),
+(18,'magenta'),
+(14,'navy blue'),
+(22,'olive'),
+(10,'orange'),
+(9,	'pink'),
+(8,	'purple'),
+(1,	'red'),
+(15,'sky blue'),
+(19,'violet'),
+(3,	'white'),
+(7,	'yellow');
+
+INSERT INTO Admin (AdminID, FirstName, LastName, Email, Username, Password, UpdatedAt) VALUES
+(1,	'Zuzana',	'Gabonayova',	'zuza0466@easv365.dk',	'sjuzn36',	'$2y$10$7vpXUuh/xG9rnINmlQqEZuz9Dr.DGWhX71iU.u61ngJ5d74exyp2y',	'2023-12-11 21:18:38'),
+(4,	'Laszlo',	'Vitkai',	'laszlo.vitkai@gmail.com',	'laszlovitkai',	'$2y$10$ykamXhn1/2sIWMMNaNeeHeQ8DZFeUCLrdLE4bTRSNZiSBxiQgi.LG',	'2023-12-11 21:42:35'),
+(5,	'Test',	'Testt',	'test@test.dk',	'test_user',	'$2y$10$9CZjR5Py.pWijtX2HJMUCOQaV8UY8QP5n0XDPoy6nK7bPJbTDIAH6',	'2023-12-12 23:43:16');
+
+INSERT INTO Product (ProductID, ProductNumber, Model, Description, Price, ProductMainImage, CategoryID, BrandID, CreatedAt, EditedAt, AdminID, StockQuantity, StripePriceID) VALUES
+(38,	'P000007',	'Air Jordan Legacy 312 Low White',	'Lorem ipsum',	999.00,	'../../assets/images/product_images/prod_6579a1e4205fa.jpeg',	1,	1,	'2023-12-13 12:21:56',	'2023-12-13 14:37:56',	NULL,	1,	'price_1OMtZPD7CQBEfsgzcRCGxBU0'),
+(40,	'P000001',	'Nike Dunk Low kids',	'Recognising the Dunk roots as the top-ranking university-team sneaker, the Be True To Your School Pack looks to the original ad campaign for inspiration. Colours represent top-flight universities, while crisp leather has the perfect amount of sheen to make em a hands-down win. So lace up and show off that varsity spirit. Are you game?',	570.00,	'../../assets/images/product_images/prod_657a04e070580.webp',	3,	1,	'2023-12-13 19:24:16',	'2023-12-14 14:41:50',	NULL,	15,	'price_1ONG6kD7CQBEfsgz2rOlau5i'),
+(41,	'P000002',	'Adidas Originals GAZELLe',	'ONE-FOR-ONE RELAUNCH OF THE BELOVED GAZELLE SHOE FROM 1991.\r\nThe original Gazelle shoes first came out as a versatile training shoe and are now a beloved streetwear classic. These junior size shoes are an exact replica of the 91s Gazelle design. They are made with a textured suede upper and vintage colors to give it authentic style.',	850.00,	'../../assets/images/product_images/prod_657a05e2d4deb.webp',	1,	2,	'2023-12-13 19:28:34',	'2023-12-14 10:21:59',	NULL,	32,	'price_1ONC3HD7CQBEfsgzACRcnTgb'),
+(42,	'P000003',	'Puma CAVEN',	'The Caven 2.0 is a subtle spin on a classic 1980s basketball silhouette for a look thats a bona fide classic â€“ courtside and beyond. Combining feelgood SoftFoam comfort with a cool retro aesthetic, these sneakers are the essence of sports-infused style.',	480.00,	'../../assets/images/product_images/prod_657a06c47b3b2.webp',	2,	3,	'2023-12-13 19:32:20',	'2023-12-13 19:32:21',	NULL,	12,	'price_1OMyALD7CQBEfsgzcuVqCONT'),
+(43,	'P000004',	'Nike Air',	'Once you take a few steps in the Nike Air Zoom Alphafly NEXT% 2, youll never look at your favorite pair of old competitive running shoes the same way again. These rocket ships are built to help shave precious time off your personal bests without giving up the foundation you need to get to the finish line. A thick, lightweight support system combines the two worlds of comfort and speed in a holy running marriage. Enjoy the maximum energy output from all our competition running shoes as you chase your personal bests.',	2499.00,	'../../assets/images/product_images/prod_657a07014d922.webp',	2,	1,	'2023-12-13 19:33:21',	'2023-12-13 19:33:21',	NULL,	27,	'price_1OMyBJD7CQBEfsgzqnAKHFhO'),
+(44,	'P000005',	'Puma SMASH 3.0 STAR GLOW',	'Smash was such a hit on the courts, weâ€™ve decided to take these classic sports shoes and deem them an all-around icon. Here, weve updated the sneaker with softer leather, an improved feel and fit, plus some glitter details.',	360.00,	'../../assets/images/product_images/prod_657a07f985706.webp',	3,	3,	'2023-12-13 19:37:29',	'2023-12-13 19:37:30',	NULL,	4,	'price_1OMyFKD7CQBEfsgzcT4CetXB'),
+(45,	'P000006',	'Tommy Hilfiger SKYLER',	'This high top sneaker from Tommy Hilfiger is the new model all sneaker fans are talking about! The textile upper and the white color give it a sporty and fashionable look. It has an insole in natural textile and an outsole in synthetic material. A cool sneaker in urban style, which is now also available in a childrens version!',	460.00,	'../../assets/images/product_images/prod_657a0880df6f9.webp',	3,	9,	'2023-12-13 19:39:44',	'2023-12-13 19:39:45',	NULL,	8,	'price_1OMyHVD7CQBEfsgz6JAN2ZBe'),
+(46,	'P000008',	'Under Armour CHARGED PURSUIT 3',	'Light and flexible is the name of the game here. A lot of mesh for breathability and Charged CushioningÂ® to help protect against impact. Just put them on and run for miles.',	520.00,	'../../assets/images/product_images/prod_657a095a6b361.webp',	1,	8,	'2023-12-13 19:43:22',	'2023-12-13 19:43:23',	NULL,	26,	'price_1OMyL0D7CQBEfsgzW5hUZ3ot'),
+(47,	'P000009',	'JORDAN ONE TAKE 4',	'Get the speed you need - just like Russ. The outsole of the Jordan One Take 4 is inspired by Russell Westbrooks latest signature shoe and goes almost up to the midsole, so you can quickly start, stop or change direction. Zoom Air shock absorption in the forefoot provides energy return and keeps you moving.',	800.00,	'../../assets/images/product_images/prod_657a0a0a3cc35.webp',	1,	10,	'2023-12-13 19:46:18',	'2023-12-13 19:46:18',	NULL,	21,	'price_1OMyNqD7CQBEfsgz0pXsUFM9'),
+(48,	'P000010',	'Vans OLD SKOOL',	'Lightness, style and comfort... These sneakers reflect all the benefits of the skater lifestyle when it comes to Vans. It is both old school and modern. Its design follows both fashion trends and our needs.',	524.00,	'../../assets/images/product_images/prod_657a0cd59b1ad.webp',	1,	6,	'2023-12-13 19:58:13',	'2023-12-13 19:58:14',	NULL,	45,	'price_1OMyZOD7CQBEfsgzzY6KAxAn');
+
+INSERT INTO ProductSize (ProductID, SizeID) VALUES
+(38,	4),
+(38,	5),
+(38,	7),
+(38,	8),
+(40,	1),
+(40,	2),
+(40,	3),
+(40,	4),
+(40,	5),
+(41,	8),
+(41,	9),
+(41,	11),
+(41,	13),
+(41,	14),
+(41,	15),
+(42,	1),
+(42,	5),
+(42,	7),
+(42,	8),
+(42,	11),
+(43,	7),
+(43,	9),
+(43,	11),
+(44,	1),
+(44,	2),
+(44,	3),
+(45,	1),
+(45,	2),
+(46,	11),
+(46,	13),
+(46,	16),
+(46,	18),
+(47,	11),
+(47,	15),
+(47,	17),
+(47,	19),
+(47,	21),
+(48,	9),
+(48,	10),
+(48,	11),
+(48,	12),
+(48,	13);
+
+INSERT INTO DailySpecialOffer (DailySpecialOfferID, ProductID) VALUES
+(1,	40);
+
+INSERT INTO ProductColor (ProductID, ColorID) VALUES
+(40,	9),
+(41,	6),
+(42,	3),
+(43,	9),
+(44,	3),
+(45,	3),
+(46,	4),
+(47,	1),
+(48,	7);
+
+INSERT INTO `orders` (`id`, `session_id`, `payment_intent_id`, `amount_total`, `currency`, `customer_id`, `customer_email`, `payment_status`, `payment_method_types`, `shipping_address`, `created_at`, `customer_name`, `customer_phone`) VALUES
+(12,	'cs_test_b1WI7fNHP7t4Oug0EILYU2q4UonIFs5SFufqf6641s36AJ6RYOw1ubboqA',	'pi_3ONEQjD7CQBEfsgz0rB0eX7M',	165000,	'dkk',	NULL,	'laszlo.vitkai@gmail.com',	'paid',	NULL,	'{\"address\":{\"city\":\"Esbjerg\",\"country\":\"DK\",\"line1\":\"Spangsbjerg Kirkevej 101 A\",\"line2\":null,\"postal_code\":\"6700\",\"state\":\"\"},\"name\":\"Laszlo Vitkai\"}',	'2023-12-14 12:54:23',	'Laszlo Vitkai',	'+4550283241'),
+(13,	'cs_test_a1uudoFOA4NXkFeswdl9hU92coTy6RHTyJ8RmWGrTt3z3rrF2PiNPYei2q',	'pi_3ONFNPD7CQBEfsgz1pjvnBF1',	85000,	'dkk',	NULL,	'gabonayova.zuzka@gmail.com',	'paid',	NULL,	'{\"address\":{\"city\":\"Esbjerg\",\"country\":\"DK\",\"line1\":\"Spangsbjerg Kirkevej 101A\",\"line2\":null,\"postal_code\":\"6700\",\"state\":\"\"},\"name\":\"Zuzana Gabonayova\"}',	'2023-12-14 13:55:03',	'Zuzana Gabonayova',	'+4555255769');
+
+INSERT INTO order_line_items (id, order_id, product_name, quantity) VALUES
+(10,	12,	'JORDAN ONE TAKE 4',	1),
+(11,	12,	'Adidas Originals GAZELLe',	1),
+(12,	13,	'Adidas Originals GAZELLe',	1);
+
+INSERT INTO dailyspecialofferlog (LogID, DailySpecialOfferID, ProductID, UpdatedAt) VALUES
+(1, 1, 41, '2023-12-14 21:17:53');
+
+
