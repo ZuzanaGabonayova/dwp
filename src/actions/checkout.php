7fa31fbee91838f17151b3b7fcb7380dc56e58c9
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-14 13:10:12 +0100

    Fix vendor/autoload.php path in checkout.php

--- src/actions/checkout.php
+++ src/actions/checkout.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-require_once __DIR__ . '../../vendor/autoload.php';
+require_once __DIR__ . '../../../vendor/autoload.php';
 require_once __DIR__ . '../config/secrets.php';
 
 \Stripe\Stripe::setApiKey($stripeSecretKey);
