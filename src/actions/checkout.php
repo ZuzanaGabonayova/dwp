b97e651e046bd7cbdc3f75bddaba28201b95c6d3
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-14 13:09:30 +0100

    Fix file path in checkout.php

--- src/actions/checkout.php
+++ src/actions/checkout.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-require_once '../../vendor/autoload.php';
-require_once '../config/secrets.php';
+require_once __DIR__ . '../../vendor/autoload.php';
+require_once __DIR__ . '../config/secrets.php';
 
 \Stripe\Stripe::setApiKey($stripeSecretKey);
 session_start();
