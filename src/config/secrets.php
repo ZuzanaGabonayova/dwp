e31ad74ffab7b95471eca2c806a26eacf4ff89f0
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-14 13:14:52 +0100

    Update path to load .env file from two directories back

--- src/config/secrets.php
+++ src/config/secrets.php
@@ -1,3 +1,10 @@
 <?php
+use Dotenv\Dotenv;
+
+require_once __DIR__ . '../../../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable('/home/master/applications/phqmbyaurd/public_html'); // Adjusted path to load .env from two directories back
+$dotenv->load();
+
 $stripeSecretKey = $_ENV['STRIPE_SECRET_KEY'] ?? null;
 ?>
