7467966f6b708b4c796abb62836707ccef58b11e
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-14 14:25:12 +0100

    Fix path to load .env file

--- src/actions/checkout.php
+++ src/actions/checkout.php
@@ -3,10 +3,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+use Dotenv\Dotenv;
+
 require_once __DIR__ . '../../../vendor/autoload.php';
 require_once __DIR__ . '../../config/secrets.php';
 
-use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable('/home/master/applications/phqmbyaurd/public_html'); // Adjusted path to load .env from two directories back
 $dotenv->load();
