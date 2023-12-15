31f13a27889468f8688ec2155e1289392051739e
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-15 11:12:36 +0100

    remove error displaying

--- src/components/admin/daily_special_offer.php
+++ src/components/admin/daily_special_offer.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', 1);   
-ini_set('display_startup_errors', 1);
+/* ini_set('display_errors', 1);   
+ini_set('display_startup_errors', 1); */
 
 require_once __DIR__ . '/../../product/ReadProductCrud.php';
 require_once __DIR__ . '/../../daily_special_offer/DailySpecialOfferCrud.php';
