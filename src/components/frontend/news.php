2cb80cf354dfd90682af692a5b26071c81742051
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-15 09:46:04 +0100

    Fix heading tag in news.php

--- src/components/frontend/news.php
+++ src/components/frontend/news.php
@@ -20,7 +20,7 @@ $newsPosts = $readNewsCrud->readAllNewsPosts();
 <section id="news-section" class="bg-white py-24 sm:py-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl text-center">
-      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">News/h2>
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">News</h2>
       <p class="mt-2 text-lg leading-8 text-gray-600">Learn about the newest trends in the world of sneakers.</p>
     </div>
     <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
