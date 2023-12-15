990ebf2d65803cc63c5256cd41dff1d3aedfb048
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-15 11:21:32 +0100

    remove error displaying

--- src/views/frontend/single_product.php
+++ src/views/frontend/single_product.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
+/* ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); */
 session_start();
 require_once __DIR__ . '/../../config/db.php';
 require_once __DIR__ . '/../../product/ReadProductCrud.php';
