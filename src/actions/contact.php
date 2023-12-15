2bca2f2adeaae1e6b82d67e3710ad82ebaf85103
ZuzanaGabonayova <gabonayova.zuzka@gmail.com>
2023-12-15 11:10:38 +0100

    remove error displaying

--- src/actions/contact.php
+++ src/actions/contact.php
@@ -1,8 +1,8 @@
 <?php
 header('Content-Type: application/json');
-ini_set('display_errors', 1);
+/* ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); */
 
 use Dotenv\Dotenv;
 use PHPMailer\PHPMailer\PHPMailer;
--- src/actions/handle_special_offer.php
+++ src/actions/handle_special_offer.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('display_errors', 1);   
-ini_set('display_startup_errors', 1);
+/* ini_set('display_errors', 1);   
+ini_set('display_startup_errors', 1); */
 
 require_once '../config/db.php';
 require_once '../daily_special_offer/DailySpecialOfferCrud.php';
