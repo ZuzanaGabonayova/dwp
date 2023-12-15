77553220751d9b141b9aaf0c10884673dbab0ecf
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2023-12-15 10:04:39 +0100

    Add home_presentation component

--- src/components/frontend/home_content_wrapper.php
+++ src/components/frontend/home_content_wrapper.php
@@ -4,5 +4,6 @@ include_once 'home_hero.php';
 include_once 'home_categories.php';
 include_once 'home_daily_special_offer.php';
 include_once 'news.php';
+include_once 'home_presentation.php';
 
 ?>
---
+++ src/components/frontend/home_presentation.php
@@ -0,0 +1,71 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '../../../config/db.php';
+require_once __DIR__ . '../../../company/ReadCompanyCrud.php';
+
+$crud = new ReadCompanyCrud($conn);
+$companyDetails = $crud->readCompanyPresentation();
+?>
+
+<div class="bg-white py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <<div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
+        <div class="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2">
+        </div>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900">About us</h2>
+        <p class="mt-6 text-lg leading-8 text-gray-600"><?php echo $companyDetails['DescriptionOfCompany']; ?></p>
+        <dl class="mt-10 space-y-4 text-base leading-7 text-gray-600">
+          <div class="flex gap-x-4">
+            <dt class="flex-none">
+              <span class="sr-only">Address</span>
+              <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+              </svg>
+            </dt>
+            <dd><?php echo $companyDetails['Street'], ', ', $companyDetails['HouseNumber'], ', ', $companyDetails['PostalCodeID']; ?></dd>
+          </div>
+          <div class="flex gap-x-4">
+            <dt class="flex-none">
+              <span class="sr-only">Telephone</span>
+              <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+              </svg>
+            </dt>
+            <dd>
+                <a class="hover:text-gray-900" href="tel:+45 <?php echo $companyDetails['Phone']; ?>">
+                    <?php echo $companyDetails['Phone']; ?>
+                </a>
+            </dd>
+          </div>
+          <div class="flex gap-x-4">
+            <dt class="flex-none">
+              <span class="sr-only">Email</span>
+              <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+              </svg>
+            </dt>
+            <dd>
+                <a class="hover:text-gray-900" href="mailto:<?php echo $companyDetails['Email']; ?>">
+                <?php echo $companyDetails['Email']; ?>
+                </a>
+            </dd>
+          </div>
+          <div class="flex gap-x-4">
+            <dt class="flex-none">
+                <span class="sr-only">Opening hours</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 w-6 text-gray-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </dt>
+            <dd>
+                <p class="hover:text-gray-900">
+                    <?php echo $companyDetails['OpeningHours']; ?>
+                </p>
+            </dd>
+        </dl>
+      </div>
+  </div>
+</div>
