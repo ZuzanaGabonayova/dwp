1c51733bbbdbb299bb78d5ef948c5b0abf41be74
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2024-01-01 14:12:17 +0100

    Add CSS stylesheet to edit_news_post.php

--- src/views/admin/edit_news_post.php
+++ src/views/admin/edit_news_post.php
@@ -29,6 +29,7 @@ updateLastActivityTime();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit News Post</title>
+    <link rel="stylesheet" href="../../../assets/css/output.css">
 </head>
 <body>
     <div class="bg-white">
