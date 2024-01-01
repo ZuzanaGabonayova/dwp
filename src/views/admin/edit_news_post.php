68859e8894e4c4f09c2ca29b41ca7706b8600a63
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2024-01-01 15:13:15 +0100

    Update input field styling in edit_news_post.php

--- src/views/admin/edit_news_post.php
+++ src/views/admin/edit_news_post.php
@@ -44,7 +44,7 @@ updateLastActivityTime();
                                 <div class="col-span-full">
                                     <label for="title" class="block text-base/6 text-zinc-950">Title:</label>
                                     <div class="mt-2">
-                                         <input type="text" id="title" name="title" value="<?php echo htmlspecialchars($newsPost['title']); ?>" required class="rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                                         <input type="text" id="title" name="title" value="<?php echo htmlspecialchars($newsPost['title']); ?>" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
                                     </div>
                                 </div>
                             </div>
